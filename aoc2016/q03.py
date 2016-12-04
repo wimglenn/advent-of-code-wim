a12da6f10df69d12e5c7fed62c1f875872cd5c4d
wim glenn <git@wimglenn.com>
2016-12-03 21:41:02 -0600

    trick to reshape using -1 (thanks AndrasDeak)

--- aoc2016/q03.py
+++ aoc2016/q03.py
@@ -3,9 +3,8 @@ import numpy as np
 
 
 A = np.fromstring(data, sep=' ', dtype=int)
-N = len(A)//3
-R = A.reshape(N, 3).T
-C = R.reshape(N, 3).T
+R = A.reshape(-1, 3).T
+C = R.reshape(-1, 3).T
 
 for A in R, C:
     T  = (A[0] + A[1] > A[2])
