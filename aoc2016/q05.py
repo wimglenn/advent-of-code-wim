81e3e6dce7ed1bf1624fb6a04f1b429fde158ddb
Wim Glenn <wglenn@jumptrading.com>
2016-12-05 10:40:47 -0600

    tidy up minor inefficiencies

--- aoc2016/q05.py
+++ aoc2016/q05.py
@@ -7,18 +7,20 @@ data = data.strip()
 code1 = ''
 code2 = [None] * 8
 n = 0
+remaining = set('01234567')
 while True:
     test = '{}{}'.format(data, n).encode('ascii')
     hash_ = md5(test).hexdigest()
-    if hash_.startswith('0'*5):
-        code1 += hash_[5]
-        if hash_[5] in '01234567':
-            pos = int(hash_[5])
-        if code2[pos] is None:
-            code2[pos] = hash_[6]
-    if None not in code2 and len(code1) > 7:
-        break
     n += 1
+    if not hash_.startswith('0'*5):
+        continue
+    h5, h6 = hash_[5], hash_[6]
+    code1 += h5
+    if h5 in remaining:
+        code2[int(h5)] = h6
+        remaining.remove(h5)
+        if not remaining:
+            break
 
 code1 = code1[:8]
 code2 = ''.join(code2)
