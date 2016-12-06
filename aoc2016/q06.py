3eee1402da71ef6ea9c06467dcacbe43caad3e4a
Wim Glenn <wglenn@jumptrading.com>
2016-12-06 11:32:37 -0600

    pure python

--- aoc2016/q06.py
+++ aoc2016/q06.py
@@ -3,10 +3,10 @@ import numpy as np
 from aocd import data
 
 
-A = np.array([list(a) for a in data.splitlines()], dtype='U1')
+columns = zip(*data.splitlines())
 
 code1 = code2 = ''
-for column in A.T:
+for column in columns:
     counter = Counter(column)
     code1 += max(counter, key=counter.get)
     code2 += min(counter, key=counter.get)
