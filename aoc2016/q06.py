19f89f0bd4fcb01167d676a10af676d4ee003be8
Wim Glenn <hey@wimglenn.com>
2016-12-06 11:43:59 -0600

    simplified

--- aoc2016/q06.py
+++ aoc2016/q06.py
@@ -1,15 +1,8 @@
 from collections import Counter
-import numpy as np
 from aocd import data
 
 
-columns = zip(*data.splitlines())
+counters = [Counter(x) for x in zip(*data.splitlines())]
+for f in max, min: 
+    print(''.join(f(c, key=c.get) for c in counters))
 
-code1 = code2 = ''
-for column in columns:
-    counter = Counter(column)
-    code1 += max(counter, key=counter.get)
-    code2 += min(counter, key=counter.get)
-
-print(code1)
-print(code2)
