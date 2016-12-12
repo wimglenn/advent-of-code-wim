db1deac83c33756eedac060ce9c75468535f5540
wim glenn <wim.glenn@gmail.com>
2016-12-12 13:24:07 -0600

    Update q12.py
    day 12

--- aoc2016/q12.py
+++ aoc2016/q12.py
@@ -9,43 +9,31 @@ jnz a 2
 dec a'''
 
 
-def compute(data, c=None):
+def compute(data, c=0):
     reg = dict.fromkeys('abcd', 0)
-    if c:
-        reg['c'] = 1
+    reg['c'] = c
     i = 0
     lines = data.splitlines()
     while i < len(lines):
         line = lines[i]
         if line.startswith('cpy'):
             a, b = line.split()[1:]
-            try:
-                a = int(a)
-            except ValueError:
-                a = reg[a]
-            reg[b] = a
+            reg[b] = reg[a] if a in reg else int(a)
         elif line.startswith('inc'):
             a = line.split()[1]
             reg[a] += 1
         elif line.startswith('dec'):
             a = line.split()[1]
             reg[a] -= 1
-        if line.startswith('jnz'):
+        elif line.startswith('jnz'):
             a, b = line.split()[1:]
-            try:
-                a = int(a)
-            except ValueError:
-                a = reg[a]
-            b = int(b)
+            a = reg[a] if a in reg else int(a)
             if a:
-                i = i + b
-            else:
-                i += 1
-        else:
-            i += 1
+                i += int(b) - 1
+        i += 1
     return reg['a']
 
 
 assert(compute(test_data) == 42)
-print(compute(data))
-print(compute(data, c=1))
+print(compute(data))  # 318007
+print(compute(data, c=1))  # 9227661
