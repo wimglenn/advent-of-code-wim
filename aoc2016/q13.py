5db0ab7f5f427723ec542e37a5e83f6970d60ced
wim glenn <git@wimglenn.com>
2016-12-15 01:46:19 -0600

    fixed day 13

--- aoc2016/q13.py
+++ aoc2016/q13.py
@@ -27,10 +27,14 @@ class Maze(object):
         (1, 1, 0, 1): '┣',
         (1, 1, 1, 0): '┫',
         (1, 1, 1, 1): '╋',
-        None: '.',  # empty space character
     }
-    wallmap = defaultdict(lambda: '#')
-    wallmap[None] = '.'
+    # wallmap = defaultdict(lambda: '#')
+    wallmap.update({
+        'open space': ' ',
+        'place seen': '.',
+        'start spot': 'o',
+        'the target': 'X',
+    })
 
     def __init__(self, fav_number=int(data)):
         self.fav_number = fav_number
@@ -53,15 +57,23 @@ class Maze(object):
             if not self.is_wall(z) and z not in seen:
                 yield z
 
-    def get_char(self, x, y):
-        if not self.is_wall(complex(x,y)):
-            return self.wallmap[None]
+    def get_char(self, x, y, visited=(), start=None, end=None):
+        state = complex(x, y)
+        if state == start:
+            # assert 0
+            return self.wallmap['start spot']
+        elif state == end:
+            return self.wallmap['the target']
+        elif state in visited:
+            return self.wallmap['place seen']
+        elif not self.is_wall(state):
+            return self.wallmap['open space']
         # up, down, left, right
-        deltas = (0, -1), (0, +1), (-1, 0), (+1, 0)
-        walls = [self.is_wall(complex(x+dx, y+dy)) for dx, dy in deltas]
+        deltas = -1j, +1j, -1, +1
+        walls = [self.is_wall(state+d) for d in deltas]
         return self.wallmap[tuple(walls)]
 
-    def display(self, w=None, h=None):
+    def display(self, w=None, h=None, visited=(), start=None, end=None):
         if not self.memo:
             # prevents max on an empty sequence
             self.is_wall(complex(50, 50))
@@ -70,18 +82,16 @@ class Maze(object):
         if h is None:
             h = max([int(x.imag) for x in self.memo]) + 1
         for y in range(h):
-            row = [self.get_char(x, y) for x in range(w)]
+            row = [self.get_char(x, y, visited=visited, start=start, end=end) for x in range(w)]
             line = ''.join(row)
             print(line)
 
 
-def bfs(state0, target, maze, verbose=False, max_depth=None):
+class MaxDepth(Exception):
+    pass
 
-    def progress_bar(msg, spinner=cycle(r'\|/-')):
-        msg = ('\r{} '.format(next(spinner)) + msg).ljust(50)
-        sys.stdout.write(msg)
-        sys.stdout.flush()
 
+def bfs(state0, target, maze, max_depth=None):
     depth = 0
     queue = deque([(state0, depth)])
     seen = {state0}
@@ -92,38 +102,29 @@ def bfs(state0, target, maze, verbose=False, max_depth=None):
         if max_depth is not None:
             if new_depth > max_depth:
                 msg = 'Aborting at depth {}'.format(max_depth)
-                if verbose:
-                    progress_bar(msg, spinner=iter('✖'))
-                    sys.stdout.write('\n')
-                err = Exception(msg)
-                err.n_visited = len(seen) - 1
+                err = MaxDepth(msg)
+                err.n_visited = len(seen) - len(queue) - 1
+                err.seen = seen
                 raise err
         if new_depth > depth:
             depth = new_depth
         if state == target:
-            if verbose:
-                progress_bar('Target found at depth {}'.format(depth), spinner=iter('✔'))
-                sys.stdout.write('\n')
             return depth
-        else:
-            if verbose and (i%200 == 1):
-                progress_bar('search depth {}, queue length {}'.format(new_depth, len(queue)))
         children = list(maze.valid_next_states(state, seen=seen))
         seen.update(children)
         queue.extend((child, depth + 1) for child in children)
 
 
-def n_points_within_distance(state0, maze, d=50, verbose=False):
+def n_points_within_distance(state0, maze, d=50):
     target = -1  # just any impossible-to-find state
     try:
-        bfs(state0, target, maze, max_depth=d, verbose=verbose)
-    except Exception as err:
+        bfs(state0, target, maze, max_depth=d)
+    except MaxDepth as err:
         return err.n_visited
 
 
 # state: complex number with (x, y) == (row, col) == (state.imag, state.real)
 state0 = 1+1j
-
 test_maze = Maze(fav_number=10)
 # test_maze.display(h=7, w=10)
 assert bfs(state0, target=7+4j, maze=test_maze) == 11
@@ -137,10 +138,10 @@ print(n_points_within_distance(state0, maze))  # part B: 124
 others_data = {
     'kevin': (1362, 82, 138),
     'davidism': (1350, 92, 124),
-    # 'dsm': (1358, 96, 141),
-    # 'andras': (1364, 86, 127),
+    'dsm': (1358, 96, 141),
+    'andras': (1364, 86, 127),
 }
 for name, (fav_number, partA, partB) in others_data.items():
     other_maze = Maze(fav_number=fav_number)
-    assert bfs(state0, target, other_maze, verbose=False) == partA
-    assert n_points_within_distance(state0, other_maze, verbose=False) == partB
+    assert bfs(state0, target, other_maze) == partA
+    assert n_points_within_distance(state0, other_maze) == partB
