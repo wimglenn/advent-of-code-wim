2ab124f4251c2f21d965efb2f12185a2bcb18a87
wim glenn <git@wimglenn.com>
2016-12-17 18:52:22 -0600

    day 8

---
+++ aoc2015/q08.py
@@ -0,0 +1,48 @@
+from aocd import data
+import ast
+
+
+test_data = r'''""
+"abc"
+"aaa\"aaa"
+"\x27"'''
+
+
+def tokens(s):
+    iterator = iter(s)
+    for char in iterator:
+        if char == '\\':
+            char += next(iterator)
+            if char.endswith('x'):
+                char += next(iterator)
+                char += next(iterator)
+        yield char
+
+
+def encoder(s):
+    iterator = iter(s)
+    yield '"'
+    for char in iterator:
+        if char == '"' or char == '\\':
+            yield '\\'
+        yield char
+    yield '"'
+
+
+def tokens_len(s):
+    return sum(1 for token in tokens(s)) - 2
+
+
+def length_diff(data):
+    return sum(len(line) - tokens_len(line) for line in data.splitlines())
+
+
+def encoded_diff(data):
+    return sum(len(''.join(encoder(line))) - len(line) for line in data.splitlines())
+
+
+assert length_diff(test_data) == 12
+print(length_diff(data))
+
+assert encoded_diff(test_data) == 19
+print(encoded_diff(data))
