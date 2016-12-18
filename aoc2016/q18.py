be1053d8a8b76693613e76079ffafc9465173be4
wim glenn <git@wimglenn.com>
2016-12-18 00:21:28 -0600

    day 18

---
+++ aoc2016/q18.py
@@ -0,0 +1,45 @@
+from aocd import data
+
+
+def generate_rows(row):
+    while True:
+        yield row
+        row = '.' + row + '.'
+        S = {'^^.', '.^^', '^..', '..^'}
+        indices = range(1, len(row)-1)
+        row = ''.join('.^'[row[i-1:i+2] in S] for i in indices)
+
+def make_grid(row0, nrows):
+    gen = generate_rows(row0)
+    return '\n'.join(next(gen) for i in range(nrows))
+
+def n_safe_tiles(row0, nrows):
+    gen = generate_rows(row0)
+    return sum(next(gen).count('.') for i in range(nrows))
+
+
+assert make_grid('..^^.', nrows=3) == '''
+..^^.
+.^^^^
+^^..^
+'''.strip()
+
+test_grid = '''
+.^^.^.^^^^
+^^^...^..^
+^.^^.^.^^.
+..^^...^^^
+.^^^^.^^.^
+^^..^.^^..
+^^^^..^^^.
+^..^^^^.^^
+.^^^..^.^^
+^^.^^^..^^
+'''.strip()
+
+row0 = test_grid.splitlines()[0]
+assert make_grid(row0, nrows=10) == test_grid
+assert n_safe_tiles(row0, nrows=10) == 38
+
+print(n_safe_tiles(data, nrows=40))  # part a: 1987
+print(n_safe_tiles(data, nrows=400000))  # part b: 19984714
