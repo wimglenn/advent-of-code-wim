a738833bbad9e25f789f52db819dcd62141b0e88
wim glenn <wim.glenn@gmail.com>
2016-12-19 19:00:50 -0600

    day 19

--- aoc2016/q19.py
+++ aoc2016/q19.py
@@ -13,10 +13,10 @@ def part_a(n):
 def part_b(n):
     n = int(n)
     elves = deque(range(1, n+1))
+    elves.rotate((n + 1)//2)
     while elves:
-        elves.rotate(-(n//2))
         elf = elves.popleft()
-        elves.rotate(n//2 - 1)
+        elves.rotate(n//2 + n//2 - 1)
         n -= 1
     return elf
 
@@ -24,32 +24,5 @@ def part_b(n):
 assert part_a(5) == 3
 assert part_b(5) == 2
 
-assert part_a(9) == 3
-assert part_b(9) == 9
-
-assert part_a(10) == 5
-assert part_b(10) == 1
-
-assert part_a(27) == 23
-assert part_b(27) == 27
-
-assert part_a(81) == 35
-assert part_b(81) == 81
-
-assert part_a(243) == 231
-assert part_b(243) == 243
-
-assert part_a(98) == 69
-assert part_b(98) == 17
-
-assert part_a(99) == 71
-assert part_b(99) == 18
-
-assert part_a(100) == 73
-assert part_b(100) == 19
-
-assert part_a(101) == 75
-assert part_b(101) == 20
-
 print(part_a(data))
-# print(part_b(data))
+print(part_b(data))
