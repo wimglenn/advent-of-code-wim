f0732a816f446c2d5501b9b375ef9a833aba1250
wim glenn <git@wimglenn.com>
2016-12-23 01:38:13 -0600

    day 15

--- aoc2015/q15.py
+++ aoc2015/q15.py
@@ -2,10 +2,27 @@ from aocd import data
 from itertools import permutations
 from collections import Counter, defaultdict
 import numpy as np
+import sys
 
 
 test_data = '''Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8
 Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3'''
+
+
+def change(total, coins=(1,5,10,25,50)):
+    if not total:
+        return [Counter()]
+    M = defaultdict(list)
+    coins = sorted(coins)
+    for row, coin in enumerate(coins, 1):
+        for amount in range(1, total + 1):
+            # progress bar here?
+            if coin == amount:
+                M[row,amount].append(Counter({coin:1}))
+            M[row,amount].extend(M[row-1,amount])
+            if coin < amount:
+                M[row,amount].extend([counter + Counter({coin:1}) for counter in M[row,amount-coin]])
+    return M[len(coins),total]
 
 
 def parse_data(data):
@@ -18,21 +35,20 @@ def parse_data(data):
 
 
 def score(R, A):
-    A = A.copy()
-    A *= R
-    A = A.sum(axis=0)
+    A = (R*A).sum(axis=0)
     A[A<0] = 0
-    score = A[:-1].prod()
-    return score
+    return A[:-1].prod()
 
 
-def get_perturbations(R, calories_target=None):
-    n = len(R)
+def get_perturbations(R0):
+    n = len(R0)
     for up, down in permutations(range(n), 2):
-        yield R + np.eye(n, 1, -up, dtype=int) - np.eye(n, 1, -down, dtype=int)
+        R = R0 + np.eye(n, 1, -up, dtype=int) - np.eye(n, 1, -down, dtype=int)
+        if (R >= 0).all():
+            yield R
 
 
-def get_best_score(data, calories_target=None):
+def get_best_score(data):
     ingredients = parse_data(data)
     A = np.vstack(ingredients.values())
     n = len(A)
@@ -41,7 +57,7 @@ def get_best_score(data, calories_target=None):
 
     best_score = 0
     while True:
-        for R_ in get_perturbations(R, calories_target=calories_target):
+        for R_ in get_perturbations(R):
             this_score = score(R_, A)
             if this_score > best_score:
                 R = R_
@@ -51,27 +67,22 @@ def get_best_score(data, calories_target=None):
             return best_score
 
 
+def get_best_score_for_calories(data, n_teaspoons=100, calories=500):
+    ingredients = parse_data(data)
+    A = np.vstack(ingredients.values())
+    n = len(A)
+    cals = A[:,-1]
+    best_score = 0
+    for combo in change(total=calories, coins=cals):
+        R = np.array([combo.get(x, 0) for x in cals]).reshape(n,1)
+        if R.sum() == n_teaspoons:
+            this_score = score(R, A)
+            best_score = max(best_score, this_score)
+    return best_score
+
+
 assert get_best_score(test_data) == 62842880
-# print(get_best_score(data))  # part a: 18965440
-assert get_best_score(data) == 18965440
+print(get_best_score(data))  # part a: 18965440
 
-# print(get_best_score(test_data, calories_target=500))
-# assert get_best_score(test_data, calories_target=500) == 57600000
-
-# print(test_data)
-print(data)
-
-
-def change(total, coins=(1,5,10,25,50)):
-    if not total:
-        return [Counter()]
-    M = defaultdict(list)
-    coins = sorted(coins)
-    for row, coin in enumerate(coins, 1):
-        for amount in range(1, total + 1):
-            if coin == amount:
-                M[row,amount].append(Counter({coin:1}))
-            M[row,amount].extend(M[row-1,amount])
-            if coin < amount:
-                M[row,amount].extend([counter + Counter({coin:1}) for counter in M[row,amount-coin]])
-    return M[len(coins),total]
+assert get_best_score_for_calories(test_data) == 57600000
+print(get_best_score_for_calories(data))  # part b: 15862900
