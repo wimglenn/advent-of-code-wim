ccab821396c01c89fbe2ca0b6cbabad5b6ef6d34
wim glenn <wim.glenn@gmail.com>
2016-12-28 09:50:27 -0600

    day 25

--- aoc2016/q25.py
+++ aoc2016/q25.py
@@ -3,7 +3,7 @@ import re
 
 def part_a(data):
     x, y = re.findall(r'\d+', data)[:2]
-    return int('0101010101010', 2) - int(x)*int(y)
+    return 0b101010101010 - int(x)*int(y)
 
 
 print(part_a(data))  # part a: 198
