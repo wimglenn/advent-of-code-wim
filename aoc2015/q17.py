8ace7d9f6ead009df45e4f5fb600cd8bc1bdc35c
wim glenn <wim.glenn@gmail.com>
2016-12-29 16:43:39 -0600

    day 17 DP

--- aoc2015/q17.py
+++ aoc2015/q17.py
@@ -1,5 +1,6 @@
 from aocd import data
 from collections import Counter
+from functools import lru_cache
 
 
 test_data = '''
@@ -11,41 +12,54 @@ test_data = '''
 '''.strip()
 
 
-def ways(total=25, containers=(5,5,10,15,20)):
+def rsubset_sum(vals, target=0):
+    # recursion impl
 
-    memo = {}
-
+    @lru_cache(maxsize=None)
     def worker(hand=frozenset()):
-        if hand not in memo:
-            sum_ = sum(containers[i] for i in hand)
-            if sum_ == total:
-                result = {hand}
-            elif sum_ > total:
-                result = set()
-            else:
-                choices = set(range(len(containers))) - hand
-                hands = (worker(hand | {choice}) for choice in choices)
-                result = set.union(*hands)
-            memo[hand] = result
-        return memo[hand]
+        sum_ = sum(vals[i] for i in hand)
+        if sum_ == target:
+            result = {hand}
+        elif sum_ > target:
+            result = set()
+        else:
+            choices = set(range(len(vals))) - hand
+            hands = (worker(hand | {choice}) for choice in choices)
+            result = set.union(*hands)
+        return result
 
-    result = [[containers[i] for i in way] for way in set(worker())]
+    result = [[vals[i] for i in way] for way in set(worker())]
     return result
 
 
-def part_a(data, total=150):
-    containers = [int(n) for n in data.splitlines()]
-    return len(ways(total=total, containers=containers))
+def subset_sum(vals, target=0):
+    # dynamic programming impl
+    sums = {0: [()]}  # key=sum, value=list of subsets for the sum
+    for val in vals:
+        items = sums.items()  # don't change dict size during iteration
+        sums = dict(items)
+        for prev_sum, prev_subsets in items:
+            sum_ = prev_sum + val
+            subsets = [s + (val,) for s in prev_subsets]
+            sums[sum_] = sums.get(sum_, []) + subsets
+            if sum_ == target:
+                yield from subsets
 
-def part_b(data, total=150):
-    containers = [int(n) for n in data.splitlines()]
-    all_ways = ways(total=total, containers=containers)
-    min_len = min([len(way) for way in all_ways])
-    return sum(1 for way in all_ways if len(way) == min_len)
+
+def part_a(vals, target, impl=subset_sum):
+    return sum(1 for subset in impl(vals, target))
+
+def part_b(vals, target, impl=subset_sum):
+    counter = Counter(len(subset) for subset in impl(vals, target))
+    return counter[min(counter)]
 
 
-assert part_a(test_data, total=25) == 4
-print(part_a(data))  # 1638
+test_vals = [int(n) for n in test_data.splitlines()]
+vals = [int(n) for n in data.splitlines()]
 
-assert part_b(test_data, total=25) == 3
-print(part_b(data))  # 17
+for impl in rsubset_sum, subset_sum:
+    assert part_a(test_vals, target=25, impl=impl) == 4
+    assert part_b(test_vals, target=25, impl=impl) == 3
+
+print(part_a(vals, target=150))  # 1638
+print(part_b(vals, target=150))  # 17
