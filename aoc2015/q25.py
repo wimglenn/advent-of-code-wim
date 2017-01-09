380c5a36b97f86414714b1f54ffdfa4997e93998
wim glenn <git@wimglenn.com>
2017-01-08 18:47:11 -0600

    day 25

---
+++ aoc2015/q25.py
@@ -0,0 +1,25 @@
+from aocd import data
+
+
+def parse_data(data):
+    words = data.split()
+    row = int(words[-3].rstrip(','))
+    col = int(words[-1].rstrip('.'))
+    return row, col
+
+
+def n(row, col):
+    i = (row+col)*(row+col) + 2 - col -3*row
+    return i//2
+
+
+def code(row, col, code0=20151125, m=252533, d=33554393):
+    i = n(row, col)
+    code = code0
+    for _ in range(i-1):
+        code = m*code % d
+    return code
+
+
+row, col = parse_data(data)
+print(code(row, col))  # 2650453
