b5ebd5f53e45e49326a4caeca9ba833145b9346d
wim glenn <hey@wimglenn.com>
2017-12-05 23:58:20 -0600

    day 6

--- aoc2017/q06.py
+++ aoc2017/q06.py
@@ -1,25 +1,23 @@
 from aocd import data
+from itertools import count
 import numpy as np
 
 def run(data):
     a = np.fromstring(data, sep=' ', dtype=int)
     n = len(a)
     seen = {}
-    i = 0
-    while True:
+    for i in count():
         t = tuple(a)
         if t in seen:
             return i, i - seen[t]
         seen[t] = i
         max_pos = a.argmax()
-        max_ = a[max_pos]
-        q, r = divmod(max_, n)
+        q, r = divmod(a[max_pos], n)
         a[max_pos] = 0
         a += q
         a = np.roll(a, -max_pos-1)
         a[:r] += 1
         a = np.roll(a, max_pos+1)
-        i += 1
 
 test_data = '0 2 7 0'
 assert run(test_data) == (5, 4)
