f417eadd5a60bb60096b305cfa652de9ad58ffb9
wim glenn <hey@wimglenn.com>
2017-12-11 11:07:43 +0400

    day 11

---
+++ aoc2017/q11.py
@@ -0,0 +1,23 @@
+from aocd import data
+from collections import Counter
+
+tests ={
+    'ne,ne,ne': 3,
+    'ne,ne,sw,sw': 0,
+    'ne,ne,s,s': 2,
+    'se,sw,se,sw,sw': 3,
+}
+
+def norm(data, steps=None):
+    c = Counter(data.split(',')[:steps])
+    c['ne'] -= c.pop('sw', 0)
+    c['nw'] -= c.pop('se', 0)
+    c['s'] -= c.pop('n', 0)
+    d = sum(abs(x) for x in c.values()) - abs(sorted(c.values())[1])
+    return d
+
+for k,v in tests.items():
+    assert norm(k) == v
+
+print(norm(data))  # part a: 794
+print(max(norm(data, steps=i) for i in range(data.count(','))))  # part b: 1524
