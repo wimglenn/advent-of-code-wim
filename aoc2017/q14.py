38a4c6dfa82b50eae1cbb41e1510d04a74a6ec3b
wim glenn <hey@wimglenn.com>
2017-12-14 17:12:31 +1100

    day 14

--- aoc2017/q14.py
+++ aoc2017/q14.py
@@ -1,12 +1,11 @@
 from aocd import data
 from q10 import knot_hash
 import numpy as np
-from skimage.morphology import label
+from scipy.ndimage.measurements import label
 
 def f(data):
-    s = ''.join(format(int(knot_hash(f'{data}-{i}'), 16), '0128b') for i in range(128))
-    a = np.fromiter(s, dtype=int).reshape(128, 128)
-    return a.sum(), label(a, connectivity=1, return_num=True)[1]
+    s = ''.join(f"{int(knot_hash(f'{data}-{i}'), 16):0128b}" for i in range(128))
+    return s.count('1'), label(np.fromiter(s, dtype=int).reshape(128, 128))[1]
 
 assert f('flqrgnkx') == (8108, 1242)
 
