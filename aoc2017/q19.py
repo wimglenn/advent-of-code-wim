a86f795e24ae6f8228be3e39a0ad45f08ac0d02d
wim glenn <hey@wimglenn.com>
2017-12-20 05:08:11 +1100

    day 19

---
+++ aoc2017/q19.py
@@ -0,0 +1,35 @@
+from aocd import data
+from itertools import count
+import numpy as np
+
+test_data = '''\
+     |          
+     |  +--+    
+     A  |  C    
+ F---|----E|--+ 
+     |  |  |  D 
+     +B-+  +--+ 
+                '''
+
+def tubemaze(data):
+    A = np.fromiter(data.replace('\n', ''), dtype='U1').reshape(len(data.splitlines()), -1)
+    x = np.array([-1, A[0].argmax()], dtype=int)
+    v = np.array([1, 0], dtype=int)
+    letters = ''
+    for i in count():
+        x += v
+        a = A[tuple(x)]
+        if a == '+':
+            v = v[::-1]
+            if A[tuple(x + v)] == ' ':
+                v *= -1
+        elif a not in '-| ':
+            letters += a
+        elif a == ' ':
+            return letters, i
+
+assert tubemaze(test_data) == ('ABCDEF', 38)
+a, b = tubemaze(data)
+
+print(a)  # part a: BPDKCZWHGT
+print(b)  # part b: 17728
