c6785e384d3285e74d7c05af504f792056c49218
wim glenn <hey@wimglenn.com>
2017-12-24 01:59:59 +1100

    day 23

---
+++ aoc2017/q23.py
@@ -0,0 +1,30 @@
+from aocd import data
+
+def run(a0=0):
+    d = dict.fromkeys('abcdefgh', 0)
+    d['a'] = a0
+    i = 0
+    n_mul = 0
+    while i < len(lines):
+        line = lines[i]
+        op, k, n = line.split()
+        n = d[n] if n in d else int(n)
+        if op == 'set':
+            d[k] = n
+        elif op == 'sub':
+            d[k] -= n
+        elif op == 'mul':
+            d[k] *= n
+            n_mul += 1
+        elif op == 'jnz':
+            k = d[k] if k in d else int(k)
+            i += n - 1 if k else 0
+        elif op == 'wtf':
+            d[k] = all(n % i for i in range(2, n))
+        i += 1
+    return d['h'] if a0 else n_mul
+
+lines = data.splitlines()
+print(run())  # part a: 5929
+lines[8:10] = ['wtf f b', 'jnz 1 15']
+print(run(a0=1))  # part b: 907
