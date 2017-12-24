2e4a32b26344b4e7b4f5bd7b06630304c8944f2a
wim glenn <hey@wimglenn.com>
2017-12-24 16:59:52 +1100

    day 24

---
+++ aoc2017/q24.py
@@ -0,0 +1,35 @@
+from aocd import data
+
+test_data = '''0/2
+2/2
+2/3
+3/4
+3/5
+0/1
+10/1
+9/10'''
+
+def get_segments(data):
+    return [tuple(int(x) for x in line.split('/')) for line in data.splitlines()]
+
+def extend(bridge, segments):
+    *rest, (connected, free) = bridge
+    end = True
+    for i, segment in enumerate(segments):
+        if free in segment:
+            end = False
+            yield from extend(bridge+[(free, segment[segment[0]==free])], segments=segments[:i] + segments[i+1:])
+    if end:
+        yield bridge
+
+def strongest_and_longest(data):
+    bridges = extend(bridge=[(0, 0)], segments=get_segments(data))
+    lengths_and_strengths = [(len(bridge), sum(x+y for x,y in bridge)) for bridge in bridges]
+    strength_strongest = max(strength for length, strength in lengths_and_strengths)
+    _length_longest, strength_longest = max(lengths_and_strengths)
+    return strength_strongest, strength_longest
+
+assert strongest_and_longest(test_data) == (31, 19)
+strongest, longest = strongest_and_longest(data)
+print(strongest)  # part a: 1940
+print(longest)  # part b: 1928
