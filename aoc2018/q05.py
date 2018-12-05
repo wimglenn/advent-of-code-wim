400fb76a75eb3effdf6e500e7512892aa081ddf0
wim glenn <hey@wimglenn.com>
2018-12-04 23:40:28 -0600

    day 5

---
+++ aoc2018/q05.py
@@ -0,0 +1,31 @@
+from aocd import data
+
+
+def part_a(data):
+    s0 = data
+    while True:
+        xs = set(s0.lower())
+        for x in xs:
+            sub1 = x + x.upper()
+            sub2 = x.upper() + x
+            s1 = s0.replace(sub1, '').replace(sub2, '')
+            if len(s1) < len(s0):
+                s0 = s1
+                break
+        else:
+            return len(s0)
+
+
+def part_b(data):
+    result = len(data)
+    for x in set(data.lower()):
+        m = part_a(data.replace(x, '').replace(x.upper(), ''))
+        result = min(m, result)
+    return result
+
+
+test_data = "dabAcCaCBAcCcaDA"
+assert part_a(test_data) == 10
+assert part_b(test_data) == 4
+print(part_a(data))  # 9370
+print(part_b(data))  # 6390
