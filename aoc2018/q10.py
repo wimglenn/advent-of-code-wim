6b56bc7264f6ee19767694119931c4d2527ccf8f
wim glenn <hey@wimglenn.com>
2018-12-10 01:08:54 -0600

    day 10

---
+++ aoc2018/q10.py
@@ -0,0 +1,76 @@
+from aocd import data
+import re
+import numpy as np
+
+
+test_data = """position=< 9,  1> velocity=< 0,  2>
+position=< 7,  0> velocity=<-1,  0>
+position=< 3, -2> velocity=<-1,  1>
+position=< 6, 10> velocity=<-2, -1>
+position=< 2, -4> velocity=< 2,  2>
+position=<-6, 10> velocity=< 2, -2>
+position=< 1,  8> velocity=< 1, -1>
+position=< 1,  7> velocity=< 1,  0>
+position=<-3, 11> velocity=< 1, -2>
+position=< 7,  6> velocity=<-1, -1>
+position=<-2,  3> velocity=< 1,  0>
+position=<-4,  3> velocity=< 2,  0>
+position=<10, -3> velocity=<-1,  1>
+position=< 5, 11> velocity=< 1, -2>
+position=< 4,  7> velocity=< 0, -1>
+position=< 8, -2> velocity=< 0,  1>
+position=<15,  0> velocity=<-2,  0>
+position=< 1,  6> velocity=< 1,  0>
+position=< 8,  9> velocity=< 0, -1>
+position=< 3,  3> velocity=<-1,  1>
+position=< 0,  5> velocity=< 0, -1>
+position=<-2,  2> velocity=< 2,  0>
+position=< 5, -2> velocity=< 1,  2>
+position=< 1,  4> velocity=< 2,  1>
+position=<-2,  7> velocity=< 2, -2>
+position=< 3,  6> velocity=<-1, -1>
+position=< 5,  0> velocity=< 1,  0>
+position=<-6,  0> velocity=< 2,  0>
+position=< 5,  9> velocity=< 1, -2>
+position=<14,  7> velocity=<-2,  0>
+position=<-3,  6> velocity=< 2, -1>"""
+
+
+def varianceish(a):
+    return a.std(axis=1)[:2].sum()
+
+
+def draw(a):
+    ps = a[:2].copy()
+    ps -= ps.min(axis=1).reshape(-1, 1)
+    h, w = ps.max(axis=1)
+    img = np.empty((w + 1, h + 1), dtype='U1')
+    img[:] = "."
+    for col, row in ps.T:
+        img[row, col] = '#'
+    print()
+    for row in img:
+        print(*row, sep='')
+    print()
+
+
+def run(data):
+    numbers = [int(x) for x in re.findall("-?\d+", data)]
+    a = np.array(numbers).reshape(-1, 4).T
+    t = 0
+    minv = varianceish(a)
+    while True:
+        a[:2] += a[2:]
+        v = varianceish(a)
+        minv = min(v, minv)
+        if v > minv:
+            a[:2] -= a[2:]
+            draw(a)
+            return t
+        t += 1
+
+
+assert run(test_data) == 3
+b = run(data)
+# part a: BLGNHPJC
+print(b)  # part b: 10476
