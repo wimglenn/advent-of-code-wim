8d7380e9b646019b0b35866d7fafb0cad584499e
wim glenn <hey@wimglenn.com>
2018-12-11 00:27:57 -0600

    day 11

--- aoc2018/q11.py
+++ aoc2018/q11.py
@@ -3,11 +3,11 @@ import numpy as np
 from scipy.signal import convolve2d
 
 
-def grid(data):
+def gen_grid(data):
     d = int(data)
     n = 300
-    a = np.empty((n,n), dtype=int)
-    v =  np.arange(0, n, dtype=int) + 1
+    a = np.empty((n, n), dtype=int)
+    v = np.arange(0, n, dtype=int) + 1
     a[:] = v + 10
     a[:] *= v.reshape(-1, 1)
     a += d
@@ -19,8 +19,8 @@ def grid(data):
 
 
 def gpl(data, x, y):
-    g = grid(data)
-    return g[y-1, x-1]
+    g = gen_grid(data)
+    return g[y - 1, x - 1]
 
 
 assert gpl(8, 3, 5) == 4
@@ -29,32 +29,35 @@ assert gpl(39, 217, 196) == 0
 assert gpl(71, 101, 153) == 4
 
 
-g0 = grid(data)
+mygrid = gen_grid(data)
 
 
-def max_power(ks, g=g0):
+def max_power(grid=mygrid, kernels=(3,)):
     maxs = []
-    for k in ks:
-        kernel = np.ones((k,k), dtype=int)
-        c = convolve2d(g, kernel, mode='valid')
+    for k in kernels:
+        kernel = np.ones((k, k), dtype=int)
+        c = convolve2d(grid, kernel, mode="valid")
         y, x = np.unravel_index(c.argmax(), c.shape)
-        maxs.append((c.max(), x+1, y+1, k))
+        maxs.append((c[y, x], x + 1, y + 1, k))
     return max(maxs)
 
 
-def part_a(g=g0):
-    m, x, y, k = max_power(ks=[3], g=g)
+def part_a(grid=mygrid):
+    m, x, y, k = max_power(grid)
     return f"{x},{y}"
 
-def part_b(g=g0):
-    m, x, y, k = max_power(ks=range(1,20), g=g)
+
+def part_b(grid=mygrid, search_limit=20):
+    m, x, y, k = max_power(grid, kernels=range(1, search_limit))
     return f"{x},{y},{k}"
 
 
-assert max_power(ks=[3], g=grid(18)) == (29, 33, 45, 3)
-assert max_power(ks=[3], g=grid(42)) == (30, 21, 61, 3)
-assert part_b(g=grid(18)) == "90,269,16"
-assert part_b(g=grid(42)) == "232,251,12"
+grid18 = gen_grid(18)
+grid42 = gen_grid(42)
+assert max_power(grid18) == (29, 33, 45, 3)
+assert max_power(grid42) == (30, 21, 61, 3)
+assert part_b(grid18) == "90,269,16"
+assert part_b(grid42) == "232,251,12"
 
 
 a = part_a()
