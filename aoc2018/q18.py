df7ca351ebd4383058ae186a6c07c12174621496
wim glenn <hey@wimglenn.com>
2018-12-18 14:33:35 -0600

    day 18

---
+++ aoc2018/q18.py
@@ -0,0 +1,63 @@
+from aocd import data
+import numpy as np
+from scipy.signal import convolve2d
+
+
+test_data = """.#.#...|#.
+.....#|##|
+.|..|...#.
+..|#.....#
+#.#|||#|#|
+...#.||...
+.|....|...
+||...#|.#|
+|.||||..|.
+...#.|..|."""
+
+
+def parsed(data):
+    h = len(data.splitlines())
+    A = np.fromiter(data.replace('\n', ''), dtype='<U1').reshape(h, -1)
+    A = (A=="|").astype(complex) + (A=="#").astype(complex)*1j
+    return A
+
+
+def dump(A):
+    d = {0: ".", 1: "|", 1j: "#"}
+    for row in A:
+        print(*[d[c] for c in row], sep='')
+    print()
+
+
+def mutate(A0):
+    k = np.ones((3, 3), dtype=complex)
+    B = convolve2d(A0, k, mode='same')
+    A1 = A0.copy()
+    A1[(A0==0) & (B.real>=3)] = 1
+    A1[(A0==1) & (B.imag>=3)] = 1j
+    A1[(A0==1j) & ((B.imag < 2) | (B.real==0))] = 0
+    return A1
+
+
+def run(data, minutes=10):
+    A = parsed(data)
+    m = minutes
+    seen = {A.tostring(): m}
+    while m > 0:
+        m -= 1
+        A = mutate(A)
+        s = A.tostring()
+        if s in seen:
+            delta = seen[s] - m
+            m %= delta
+        seen[s] = m
+    return (A==1).sum() * (A==1j).sum()
+
+
+assert run(test_data, minutes=10) == 1147
+
+a = run(data, minutes=10)
+print(a)  # 603098
+
+b = run(data, minutes=1000000000)
+print(b)  # 210000
