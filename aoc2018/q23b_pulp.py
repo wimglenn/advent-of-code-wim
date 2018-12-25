15d99e61179a35ed1dbd4cb0f860bd007aa5570a
wim glenn <hey@wimglenn.com>
2018-12-25 00:26:36 -0600

    PuLP

--- aoc2018/q23b_pulp.py
+++ aoc2018/q23b_pulp.py
@@ -6,11 +6,8 @@ import pulp
 
 template = "pos=<{:d},{:d},{:d}>, r={:d}"
 bots = [parse(template, s).fixed for s in data.splitlines()]
-N = len(bots)
-
 prob = pulp.LpProblem("problem", pulp.LpMaximize)
-
-cs = [pulp.LpVariable(f"c{i}", lowBound=0, upBound=1, cat="Integer") for i in range(N)]
+cs = [pulp.LpVariable(f"c{i}", lowBound=0, upBound=1, cat="Integer") for i in range(len(bots))]
 x = pulp.LpVariable("x")
 y = pulp.LpVariable("y")
 z = pulp.LpVariable("z")
@@ -21,9 +18,8 @@ prob += n == sum(cs)
 for i, (xi, yi, zi, r) in enumerate(bots):
     ci = cs[i]
     for s in product([-1, 1], repeat=3):
-        prob += (s[0] * (x - xi) + s[1] * (y - yi) + s[2] * (z - zi)) <= r + (
-            1 - ci
-        ) * 10**10
+        d = s[0] * (x - xi) + s[1] * (y - yi) + s[2] * (z - zi)
+        prob += d <= r + (1 - ci) * 10**10
 
 status = prob.solve()
 assert pulp.LpStatus[status] == "Optimal"
