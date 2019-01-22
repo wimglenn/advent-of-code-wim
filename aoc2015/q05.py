f4f1cc2a1e5d4d37208a0f4a443a354344e2700f
wim glenn <hey@wimglenn.com>
2019-01-21 19:32:01 -0600

    ok

--- aoc2015/q05.py
+++ aoc2015/q05.py
@@ -18,10 +18,6 @@ def blacklisted(s):
             return True
 
 
-def is_nice1(s):
-    return vowel_count(s) >= 3 and has_double(s) and not blacklisted(s)
-
-
 def has_pair(s):
     for left, right in zip(s[:-1], s[1:]):
         if s.count(left + right) > 1:
@@ -34,28 +30,24 @@ def has_skip_repeat(s):
             return True
 
 
-def is_nice2(s):
+def is_nice_a(s):
+    return vowel_count(s) >= 3 and has_double(s) and not blacklisted(s)
+
+
+def is_nice_b(s):
     return has_pair(s) and has_skip_repeat(s)
 
 
-def part_a(data):
-    return sum(1 for word in data.splitlines() if is_nice1(word))
+assert is_nice_a("ugknbfddgicrmopn")
+assert is_nice_a("aaa")
+assert not is_nice_a("jchzalrnumimnmhp")
+assert not is_nice_a("haegwjzuvuyypxyu")
+assert not is_nice_a("dvszwmarrgswjxmb")
+assert is_nice_b("qjhvhtzxzqqjkmpb")
+assert is_nice_b("xxyxx")
+assert not is_nice_b("uurcxstgmygtbstg")
+assert not is_nice_b("ieodomkazucvgmuy")
 
-
-def part_b(data):
-    return sum(1 for word in data.splitlines() if is_nice2(word))
-
-
-assert is_nice1("ugknbfddgicrmopn")
-assert is_nice1("aaa")
-assert not is_nice1("jchzalrnumimnmhp")
-assert not is_nice1("haegwjzuvuyypxyu")
-assert not is_nice1("dvszwmarrgswjxmb")
-assert is_nice2("qjhvhtzxzqqjkmpb")
-assert is_nice2("xxyxx")
-assert not is_nice2("uurcxstgmygtbstg")
-assert not is_nice2("ieodomkazucvgmuy")
-
-
-print("part a:", part_a(data))
-print("part b:", part_b(data))
+words = data.splitlines()
+print("part a:", len([w for w in words if is_nice_a(w)]))
+print("part b:", len([w for w in words if is_nice_b(w)]))
