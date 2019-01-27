81eb09dd8e8be5881ad582c3119e1153d4964082
wim glenn <hey@wimglenn.com>
2019-01-26 22:40:27 -0600

    cheap py2 support

--- aoc/__init__.py
+++ aoc/__init__.py
@@ -1,15 +1,17 @@
+import io
 import runpy
 import sys
-from io import StringIO
-from contextlib import redirect_stdout
 
 
 def wim(year, day, data):
-    mod_name = f"aoc{year}.q{day:02d}"
+    mod_name = "aoc{}.q{:02d}".format(year, day)
     sys.modules.pop(mod_name, None)
-    out = StringIO()
-    with redirect_stdout(out):
+    old_stdout = sys.stdout
+    sys.stdout = out = io.BytesIO() if sys.version_info.major == 2 else io.StringIO()
+    try:
         runpy.run_module(mod_name, run_name="__main__")
+    finally:
+        sys.stdout = old_stdout
     output_lines = [x for x in out.getvalue().splitlines() if x]
     if len(output_lines) == 2:
         part_a, part_b = output_lines
@@ -21,6 +23,10 @@ def wim(year, day, data):
         part_b = next((s for s in output_lines if s.lower().startswith("part b:")), None)
     if part_a and part_a.lower().startswith("part a:"):
         part_a = part_a[7:].strip()
+    if part_a and part_a.lower().startswith("('part a:', "):
+        part_a = part_a[12:-1].strip()
     if part_b and part_b.lower().startswith("part b:"):
         part_b = part_b[7:].strip()
+    if part_b and part_b.lower().startswith("('part b:', "):
+        part_b = part_b[12:-1].strip()
     return part_a, part_b
