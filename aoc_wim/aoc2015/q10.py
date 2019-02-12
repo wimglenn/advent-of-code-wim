1534c2bc3cae81c463439b14d8e52433452250a9
wim glenn <hey@wimglenn.com>
2019-02-11 18:30:50 -0600

    It's look and SAY not look and SEE, durrr

--- aoc_wim/aoc2015/q10.py
+++ aoc_wim/aoc2015/q10.py
@@ -2,17 +2,17 @@ from aocd import data
 from itertools import groupby
 
 
-def look_and_see(s, n=1):
+def look_and_say(s, n=1):
     for i in range(n):
         s = ''.join([f"{len(list(group))}{key}" for key, group in groupby(s)])
     return s
 
 
-assert look_and_see("211") == '1221'
-assert look_and_see('1', n=5) == '312211'
+assert look_and_say("211") == '1221'
+assert look_and_say('1', n=5) == '312211'
 
-a = look_and_see(data, n=40)
-print(len(a))
+a = look_and_say(data, n=40)
+print("part a:", len(a))
 
-b = look_and_see(a, n=50-40)
-print(len(b))
+b = look_and_say(a, n=50-40)
+print("part b:", len(b))
