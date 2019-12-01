1964ce1d2fc34ea94e6a7b8d03e0f0eb0782725e
wim glenn <hey@wimglenn.com>
2019-12-01 00:06:57 -0600

    day 1

---
+++ aoc_wim/aoc2019/q01.py
@@ -0,0 +1,30 @@
+from aocd import data
+
+import numpy as np
+
+
+def fuel(data, part="a"):
+    A = np.fromstring(data, dtype=int, sep="\n")
+    total = 0
+    while A.any():
+        A //= 3
+        A -= 2
+        A[A < 0] = 0
+        total += A.sum()
+        if part == "a":
+            break
+    return total
+
+
+test_data = """
+12
+14
+1969
+100756
+"""
+
+assert fuel(test_data) == sum([2, 2, 654, 33583])
+assert fuel(test_data, part="b") == sum([2, 2, 966, 50346])
+
+print(fuel(data))
+print(fuel(data, part="b"))
