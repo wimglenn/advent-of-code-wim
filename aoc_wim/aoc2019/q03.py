5a201b6b0abf91df6668cd32ae49540ded02d5c0
wim glenn <hey@wimglenn.com>
2019-12-03 00:08:19 -0600

    day 3

--- aoc_wim/aoc2019/q03.py
+++ aoc_wim/aoc2019/q03.py
@@ -1,15 +1,6 @@
 from aocd import data
 
 
-tests = {
-    "R8,U5,L5,D3\nU7,R6,D4,L4": (6, 30,),
-    "R75,D30,R83,U83,L12,D49,R71,U7,L72\n"
-    "U62,R66,U55,R34,D71,R55,D58,R83": (159, 610,),
-    "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\n"
-    "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7": (135, 410,),
-}
-
-
 directions = {
     "R": 1j ** 0,
     "U": 1j ** 1,
@@ -39,10 +30,17 @@ def solve(data, part="a"):
     return result
 
 
+tests = {
+    "R8,U5,L5,D3\nU7,R6,D4,L4": (6, 30,),
+    "R75,D30,R83,U83,L12,D49,R71,U7,L72\n"
+    "U62,R66,U55,R34,D71,R55,D58,R83": (159, 610,),
+    "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\n"
+    "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7": (135, 410,),
+}
+
 for test_data, (a, b) in tests.items():
     assert solve(test_data, part="a") == a
     assert solve(test_data, part="b") == b
-
 
 print(solve(data, part="a"))
 print(solve(data, part="b"))
