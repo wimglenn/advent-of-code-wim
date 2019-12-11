430f5dd0060ea3d4395c131fa2ae9cba6e2eb20e
wim glenn <hey@wimglenn.com>
2019-12-11 00:30:38 -0600

    ocr on sparse complex grids

--- aoc_wim/ocr.py
+++ aoc_wim/ocr.py
@@ -343,7 +343,7 @@ glyphs = """
 
 glyphs = glyphs.strip().split("\n\n\n")
 known = dict(zip(glyphs, "AABBCCEEFFGGHHHIIJJKKLLNOPPRRSUXZZ"))
-known_dims = Counter()
+known_dims = Counter()  # (height, width): count
 for glyph in glyphs:
     lines = glyph.splitlines()
     [w] = {len(x) for x in lines}
@@ -352,7 +352,6 @@ for glyph in glyphs:
 
 
 class DebugDict(dict):
-
     def __getitem__(self, item):
         if isinstance(item, np.ndarray) and item.ndim == 2:
             H, W = item.shape
@@ -366,9 +365,23 @@ class DebugDict(dict):
                     n, rem = divmod(W, w)
                     if rem == 0:
                         # could be a word? retry OCR per character
-                        chars = [item[:, w*i:w*(i+1)] for i in range(n)]
+                        chars = [item[:, w * i : w * (i + 1)] for i in range(n)]
                         txt = "".join([self[char] for char in chars])
                         return txt
+        if isinstance(item, dict) and {type(k) for k in item} == {complex}:
+            # sparse grid
+            sparse = np.array([(int(k.imag), int(k.real), v) for k, v in item.items()])
+            h, w, _ = sparse.ptp(axis=0)
+            full = np.zeros((h + 1, w + 1), dtype=int)
+            rows, cols, vals = sparse.T
+            full[rows - rows.min(), cols - cols.min()] = vals
+            # trim fat from left
+            while not full[:, 0].any():
+                full = full[:, 1:]
+            # trim fat from right
+            while not full[:, -2:].any():
+                full = full[:, :-1]
+            return self.__getitem__(np.flipud(full))
         if isinstance(item, np.ndarray):
             self.__missing__(item)
         return super(DebugDict, self).__getitem__(item)
