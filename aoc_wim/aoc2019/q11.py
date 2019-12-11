f8677870245cbcc051ab0399bbfdbbcdacfc3c52
wim glenn <hey@wimglenn.com>
2019-12-11 04:14:07 -0600

    fix bug :)

--- aoc_wim/aoc2019/q11.py
+++ aoc_wim/aoc2019/q11.py
@@ -1,27 +1,26 @@
 from aocd import data
 from aoc_wim.aoc2019 import IntComputer
 from aoc_wim.ocr import AOCR
-from collections import defaultdict
 from collections import deque
 
 
 class Robot:
     def __init__(self, data, colour0=0, position0=0j, direction0=1j):
-        self.brain = IntComputer(data)
+        self.brain = IntComputer(data, inputs=[colour0])
         self.brain.output = deque(maxlen=2)
         self.position = position0
         self.direction = direction0
-        self.painted = defaultdict(int, {position0: colour0})
+        self.painted = {}
 
     def paint(self):
         while True:
-            self.brain.input.append(self.painted[self.position])
             self.brain.run(until=IntComputer.op_output)
             self.brain.run(until=IntComputer.op_output)
             turn, colour = self.brain.output
             self.painted[self.position] = colour
             self.direction *= [1j, -1j][turn]
             self.position += self.direction
+            self.brain.input.append(self.painted.get(self.position, 0))
 
     def paint_until_halt(self):
         try:
