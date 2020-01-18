8ce4b3c56ae5b8b4aa4104367ed2c0b713700351
wim glenn <hey@wimglenn.com>
2020-01-18 16:57:00 -0600

    data driven testing

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -10,16 +10,16 @@ here = pathlib.Path(__file__).parent
 data_samples = sorted(here.joinpath("data").glob("*.txt"))
 
 
-@pytest.mark.parametrize("example_path", data_samples, ids=attrgetter("name"))
-def test_example(example_path, monkeypatch):
+@pytest.mark.parametrize("input_file", data_samples, ids=attrgetter("name"))
+def test_example(input_file, monkeypatch):
     # the example input filename is e.g. YYYY_dd_suffix.txt
-    year, day, *rest = str(example_path.name).split("_")
+    year, day, *rest = str(input_file.name).split("_")
     year = int(year)
     day = int(day)
 
     # the head of each example file is an input data
     # the last two lines are part a and part b correct answers
-    *lines, part_a_answer, part_b_answer = example_path.read_text().splitlines()
+    *lines, part_a_answer, part_b_answer = input_file.read_text().splitlines()
     input_data = "\n".join(lines).strip()
 
     # patch out aocd
