803308d3c31983c0408a4e1aad548052ce513cd1
wim glenn <hey@wimglenn.com>
2020-01-19 21:57:41 -0600

    data driven testing

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -8,6 +8,8 @@ from aoc_wim import plugin
 
 here = pathlib.Path(__file__).parent
 data_samples = sorted(here.joinpath("data").glob("*.txt"))
+slow = {"2015_04_a.txt", "2015_04_b.txt"}
+data_samples = [p for p in data_samples if p.name not in slow]
 
 
 @pytest.mark.parametrize("input_file", data_samples, ids=attrgetter("name"))
