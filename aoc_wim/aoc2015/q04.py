edb36b16a7c449565775537d8452dae809ba2876
wim glenn <hey@wimglenn.com>
2020-01-20 15:16:52 -0600

    data driven testing

--- aoc_wim/aoc2015/q04.py
+++ aoc_wim/aoc2015/q04.py
@@ -6,11 +6,13 @@ from aocd import data
 def mine(data, difficulty, start=0):
     secret_key = data.encode()
     prefix = "0" * difficulty
+    h0 = hashlib.md5(secret_key).copy
     i = start
     while True:
         i += 1
-        hash_ = hashlib.md5(b"%s%d" % (secret_key, i)).hexdigest()
-        if hash_.startswith(prefix):
+        hash_ = h0()
+        hash_.update(b"%d" % i)
+        if hash_.hexdigest().startswith(prefix):
             return i
 
 
