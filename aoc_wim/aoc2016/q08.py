b52cae34929a4c58e19407a757b12ec92048e093
wim glenn <hey@wimglenn.com>
2020-01-26 01:41:43 -0600

    day 08

--- aoc_wim/aoc2016/q08.py
+++ aoc_wim/aoc2016/q08.py
@@ -3,29 +3,22 @@ from aocd import data
 from aoc_wim.ocr import AOCR
 
 
-def parsed(data):
-    W, H = 50, 6
-    A = np.zeros((H, W), dtype=bool)
-    for line in data.splitlines():
-        if line.startswith("rect"):
-            w, h = [int(x) for x in line.split()[1].split("x")]
-            A[0:h, 0:w] = True
-        elif line.startswith("rotate"):
-            i, shift = [int(x) for x in line.split("=")[1].split(" by ")]
-            item = (i, slice(None)) if "row" in line else (slice(None), i)
-            A[item] = np.roll(A[item], shift)
+def animate(A, line):
+    if line.startswith("rect"):
+        w, h = [int(x) for x in line.split()[1].split("x")]
+        A[0:h, 0:w] = "#"
+    elif line.startswith("rotate"):
+        i, shift = [int(x) for x in line.split("=")[1].split(" by ")]
+        item = (i, slice(None)) if "row" in line else (slice(None), i)
+        A[item] = np.roll(A[item], shift)
     return A
 
 
-def part_a(data):
-    A = parsed(data)
-    return A.sum()
+A = np.full((6, 50), ".")
+for line in data.splitlines():
+    A = animate(A, line)
 
 
-def part_b(data):
-    A = parsed(data)
-    return AOCR[A]
-
-
-print("part a:", part_a(data))
-print("part b:", part_b(data))
+if __name__ == "__main__":
+    print("part a:", (A == "#").sum())
+    print("part b:", AOCR[A])
---
+++ tests/test_two_factor_authentication.py
@@ -0,0 +1,37 @@
+from aoc_wim.aoc2016 import q08
+from aoc_wim.zgrid import ZGrid
+import numpy as np
+
+
+frames = """\
+###....
+###....
+.......
+
+#.#....
+###....
+.#.....
+
+....#.#
+###....
+.#.....
+
+.#..#.#
+#.#....
+.#.....
+"""
+
+data = """\
+rect 3x2
+rotate column x=1 by 1
+rotate row y=0 by 4
+rotate column x=1 by 1
+"""
+
+
+def test_animate():
+    A = np.full((3, 7), ".")
+    for frame, line in zip(frames.split("\n\n"), data.splitlines()):
+        A = q08.animate(A, line)
+        expected = np.array(ZGrid(frame))
+        assert (A == expected).all()
