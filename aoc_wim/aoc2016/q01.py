a2febde845a6b65354ca73e9d386a427d91e6078
wim glenn <hey@wimglenn.com>
2020-01-25 23:00:13 -0600

    day 01

--- aoc_wim/aoc2016/q01.py
+++ aoc_wim/aoc2016/q01.py
@@ -1,51 +1,19 @@
-import numpy as np
 from aocd import data
 
 
-turn_left = np.array([[0, -1], [1, 0]], dtype=int)  # pi/2 counter-clockwise rotation
-turn_right = turn_left @ turn_left @ turn_left  # 3*pi/2 counter-clockwise rotation
+z = 0
+dz = -1j
+seen = {z}
+turns = {"R": 1j, "L": -1j}
+b = None
+for step in data.split(", "):
+    turn, n_blocks = step[0], int(step[1:])
+    dz *= turns[turn]
+    for block in range(n_blocks):
+        z += dz
+        if b is None and z in seen:
+            b = int(abs(z.real) + abs(z.imag))
+        seen.add(z)
 
-turns = {
-    "L": turn_left,
-    "R": turn_right,
-}
-
-
-def part_a(data):
-    position = np.array([0, 0], dtype=int)
-    bearing = np.array([0, 1], dtype=int)  # north
-    for step in data.split(", "):
-        turn, n_blocks = turns[step[0]], int(step[1:])
-        bearing = turn @ bearing
-        for block in range(n_blocks):
-            position += bearing
-    return abs(position).sum()
-
-
-def part_b(data):
-    position = np.array([0, 0], dtype=int)
-    bearing = np.array([0, 1], dtype=int)  # north
-    seen = {(0, 0)}
-    for step in data.split(", "):
-        turn, n_blocks = turns[step[0]], int(step[1:])
-        bearing = turn @ bearing
-        for block in range(n_blocks):
-            position += bearing
-            if tuple(position) in seen:
-                return abs(position).sum()
-            seen.add(tuple(position))
-
-
-tests = {
-    "R2, L3": 5,
-    "R2, R2, R2": 2,
-    "R5, L5, R5, R3": 12,
-}
-for test_data, expected in tests.items():
-    assert part_a(test_data) == expected
-
-assert part_b("R8, R4, R4, R8") == 4
-
-
-print("part a:", part_a(data))
-print("part b:", part_b(data))
+print("part a:", int(abs(z.real) + abs(z.imag)))
+print("part b:", b)
