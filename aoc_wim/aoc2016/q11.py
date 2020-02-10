8e68a68ce123fc4a5ff4b224de14358c8a92a9b8
wim glenn <hey@wimglenn.com>
2020-02-09 23:07:44 -0600

    day 11

--- aoc_wim/aoc2016/q11.py
+++ aoc_wim/aoc2016/q11.py
@@ -1,17 +1,8 @@
-import sys
-from collections import deque
 from itertools import combinations
-from itertools import cycle
 from itertools import product
 
 from aocd import data
-
-
-test_data = """\
-The first floor contains a hydrogen-compatible microchip and a lithium-compatible microchip.
-The second floor contains a hydrogen generator.
-The third floor contains a lithium generator.
-The fourth floor contains nothing relevant."""
+from aoc_wim.search import AStar
 
 
 def parsed(data):
@@ -42,7 +33,7 @@ def parsed(data):
         tuple(chips[k] for k in chip_names),
         tuple(generators[k] for k in chip_names),
     )
-    target = 4, tuple(4 for chip in chip_names), tuple(4 for generator in generators)
+    target = 4, tuple(4 for c in chip_names), tuple(4 for g in generators)
     if not is_valid(state0) or not is_valid(target):
         raise Exception("parsed state vector is invalid")
     return state0, target
@@ -86,42 +77,54 @@ def get_valid_next_states(state, seen=()):
             yield new_state
 
 
-def bfs(state0, target, verbose=False):
-    def progress_bar(msg, spinner=cycle(r"\|/-")):
-        msg = ("\r{} ".format(next(spinner)) + msg).ljust(50)
-        sys.stdout.write(msg)
-        sys.stdout.flush()
+class Q11AStar(AStar):
 
-    depth = 0
-    queue = deque([(state0, depth)])
-    seen = {state0}
-    i = 0
-    while queue:
-        state, new_depth = queue.popleft()
-        i += 1
-        if new_depth > depth:
-            depth = new_depth
-        if state == target:
-            if verbose:
-                msg = "Target found at depth {}".format(depth)
-                progress_bar(msg, spinner=iter("✔"))
-                sys.stdout.write("\n")
-            return depth
-        else:
-            if verbose and (i % 2000 == 0):
-                msg = "search depth {}, queue length {}".format(new_depth, len(queue))
-                progress_bar(msg)
-        children = list(get_valid_next_states(state, seen))
-        seen.update(children)
-        queue.extend((child, depth + 1) for child in children)
+    def __init__(self, data, part="a"):
+        state0, target = parsed(data)
+        if part == "b":
+            state0 = state0[0], state0[1] + (1, 1), state0[2] + (1, 1)
+            target = target[0], target[1] + (4, 4), target[2] + (4, 4)
+        AStar.__init__(self, state0, target)
+
+    def heuristic(self, state0, state1):
+        elevator0, chips0, generators0 = state0
+        elevator1, chips1, generators1 = state0
+        result = abs(elevator0 - elevator1)
+        for c0, c1 in zip(chips0, chips1):
+            result += abs(c0 - c1)
+        for g0, g1 in zip(generators0, generators1):
+            result += abs(g0 - g1)
+        return result
+
+    def adjacent(self, state):
+        elevator, chips, generators = state
+        items = list(chips + generators)
+        # indices of stuff on the same floor as the elevator:
+        indices = [i for i, item_pos in enumerate(items) if elevator == item_pos]
+        # we can take 1 or 2 things with us when changing floors
+        indices_choices = list(combinations(indices, 1)) + list(combinations(indices, 2))
+        # we can go up or down a floor
+        directions = -1, +1
+        for direction, indices in product(directions, indices_choices):
+            new_elevator = elevator + direction
+            new_items = items[:]
+            for index in indices:
+                new_items[index] += direction
+            new_chips = new_items[: len(chips)]
+            new_generators = new_items[len(chips):]
+            assert len(new_chips) == len(chips)
+            assert len(new_generators) == len(generators)
+            new_state = new_elevator, tuple(new_chips), tuple(new_generators)
+            if is_valid(new_state):
+                yield new_state
+
+    cost = heuristic
 
 
-assert bfs(*parsed(test_data), verbose=False) == 11
+a = Q11AStar(data, part="a")
+a.run()
+print("part a:", a.path_length)
 
-state0, target = parsed(data)
-print(bfs(state0, target))
-
-# put two new items on ground floor for part B
-state0 = state0[0], state0[1] + (1, 1), state0[2] + (1, 1)
-target = target[0], target[1] + (4, 4), target[2] + (4, 4)
-print(bfs(state0, target))
+b = Q11AStar(data, part="b")
+b.run()
+print("part b:", b.path_length)
