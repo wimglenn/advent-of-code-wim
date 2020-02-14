8b6b4454eb278239e2388e6b70842649d0a0a14b
wim glenn <hey@wimglenn.com>
2020-02-14 00:55:25 -0600

    day 11

--- aoc_wim/aoc2016/q11.py
+++ aoc_wim/aoc2016/q11.py
@@ -22,7 +22,7 @@ def parsed(data):
                 chip, compatible = words[i].split("-")
                 chips[chip] = line_no
     assert sorted(chips) == sorted(generators), "chip and generators mismatched"
-    pairs = sorted([(chips[k], generators[k]) for k in chips])
+    pairs = [(chips[k], generators[k]) for k in chips]
     state0 = sum(pairs, (1,))
     assert is_valid(state0), "initial state vector is invalid"
     return state0
@@ -52,30 +52,23 @@ class Q11AStar(AStar):
     def heuristic(self, state0, state1):
         return sum([abs(a - b) for (a, b) in zip(state0, state1)])
 
-    def adjacent(self, state):
-        elevator = state[0]
-        chips = state[1::2]
-        generators = state[2::2]
-        items = list(chips + generators)
-        # indices of stuff on the same floor as the elevator:
-        indices = [i for i, item_pos in enumerate(items) if elevator == item_pos]
-        # we can take 1 or 2 things with us when changing floors
-        indices_choices = list(combinations(indices, 1)) + list(combinations(indices, 2))
-        # we can go up or down a floor
-        directions = {1: (+1,), 2: (-1, +1), 3: (-1, +1), 4: (-1,)}[elevator]
-        for direction, indices in product(directions, indices_choices):
-            new_elevator = elevator + direction
-            new_items = items[:]
-            for index in indices:
-                new_items[index] += direction
-            new_chips = new_items[: len(chips)]
-            new_generators = new_items[len(chips):]
-            new_pairs = sorted([p for p in zip(new_chips, new_generators)])
-            new_state = sum(new_pairs, (new_elevator,))
-            if is_valid(new_state):
-                yield new_state
-
     cost = heuristic
+
+    def adjacent(self, state):
+        # indices of stuff on the same floor as the elevator
+        idx = [i for i in range(1, len(state)) if state[0] == state[i]]
+        # we can take 1 or 2 things with us when changing floors
+        idx_choices = [*combinations(idx, 1), *combinations(idx, 2)]
+        # we can go up or down a floor, unless on ground (1) or top (4)
+        s0s = {1: (2,), 4: (3,)}.get(state[0], (state[0] - 1, state[0] + 1))
+        for s0, idx in product(s0s, idx_choices):
+            next_state = [s0] + list(state[1:])
+            for i in idx:
+                next_state[i] = s0
+            pairs = zip(next_state[1::2], next_state[2::2])
+            next_state = sum(sorted(pairs), (s0,))
+            if is_valid(next_state):
+                yield next_state
 
 
 a = Q11AStar(data, part="a")
