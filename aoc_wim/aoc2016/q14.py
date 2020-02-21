4b9367272d6692f2b20889a4d4f8627217207d2f
wim glenn <hey@wimglenn.com>
2020-02-21 00:56:39 -0600

    day 14

--- aoc_wim/aoc2016/q14.py
+++ aoc_wim/aoc2016/q14.py
@@ -1,18 +1,10 @@
 import re
 from collections import defaultdict
-from hashlib import md5
-
 from aocd import data
-
-
-def get_first_triple(hash_):
-    candidate = re.search(r"([0-9a-f])\1{2}", hash_)
-    if candidate is not None:
-        return candidate.group()[0]
-
-
-def get_all_quintuples(hash_):
-    return re.findall(r"([0-9a-f])\1{4}", hash_)
+try:
+    from _md5 import md5
+except ImportError:
+    from hashlib import md5
 
 
 def normal_hash(s):
@@ -21,33 +13,31 @@ def normal_hash(s):
 
 def stretched_hash(s):
     for i in range(2017):
-        s = md5(s).hexdigest().encode("ascii")
-    return s.decode("ascii")
+        s = md5(s).hexdigest().encode()
+    return s.decode()
 
 
 def search(data=data, hash_function=normal_hash):
-    template = data.strip().encode("ascii") + b"%d"
+    template = data.encode() + b"%d"
     keys = []
     triples = defaultdict(list)
+    pat3 = re.compile(r"([0-9a-f])\1{2}")
+    pat5 = re.compile(r"([0-9a-f])\1{4}")
     i = 0
     stop = None
     while stop is None or i < stop:
         hash_ = hash_function(template % i)
-        for k in get_all_quintuples(hash_):
-            keys.extend([x for x in triples[k] if i - x <= 1000])
-            triples[k].clear()  # avoid to count same key twice
-            if len(keys) >= 64 and stop is None:
-                stop = i + 1001
-        triple = get_first_triple(hash_)
+        triple = pat3.search(hash_)
         if triple is not None:
-            triples[triple].append(i)
+            for quintuple in pat5.findall(hash_):
+                keys.extend([x for x in triples[quintuple] if i - x <= 1000])
+                triples[quintuple].clear()  # avoid to count same key twice
+                if stop is None and len(keys) >= 64:
+                    stop = i + 1001
+            triples[triple.group()[0]].append(i)
         i += 1
     return sorted(keys)[:64][-1]
 
 
-assert search(data="abc") == 22728
 print("part a:", search())
-
-assert stretched_hash(b"abc0") == "a107ff634856bb300138cac6568c0f24"
-assert search(data="abc", hash_function=stretched_hash) == 22551
 print("part b:", search(hash_function=stretched_hash))
