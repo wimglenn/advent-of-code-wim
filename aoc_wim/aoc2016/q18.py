71b6cf44826cc23223a049c566b766530cac6d17
wim glenn <hey@wimglenn.com>
2020-02-25 01:34:41 -0600

    day 19

--- aoc_wim/aoc2016/q18.py
+++ aoc_wim/aoc2016/q18.py
@@ -3,21 +3,20 @@ from aocd import data
 
 def generate_rows(row):
     idx = range(len(row))
-    S = {"^^.", ".^^", "^..", "..^"}
     while True:
         yield row
         row = "." + row + "."
-        row = "".join([".^"[row[i:i + 3] in S] for i in idx])
+        row = "".join(["^."[row[i] == row[i + 2]] for i in idx])
 
 
 def make_grid(row0, nrows):
     gen = generate_rows(row0)
-    return "".join(next(gen) + "\n" for i in range(nrows))
+    return "".join([next(gen) + "\n" for i in range(nrows)])
 
 
 def n_safe_tiles(row0, nrows):
     gen = generate_rows(row0)
-    return sum(next(gen).count(".") for i in range(nrows))
+    return sum([next(gen).count(".") for i in range(nrows)])
 
 
 if __name__ == "__main__":
