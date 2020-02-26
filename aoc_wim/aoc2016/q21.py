43be3ae4b330e89434ff28f7c4f0e86bbebb41c0
wim glenn <hey@wimglenn.com>
2020-02-25 23:56:21 -0600

    day 21

--- aoc_wim/aoc2016/q21.py
+++ aoc_wim/aoc2016/q21.py
@@ -3,17 +3,6 @@ from itertools import permutations
 from aocd import data
 
 
-test_data = """\
-swap position 4 with position 0
-swap letter d with letter b
-reverse positions 0 through 4
-rotate left 1 step
-move position 1 to position 4
-move position 3 to position 0
-rotate based on position of letter b
-rotate based on position of letter d"""
-
-
 def swap_position(list_, words):
     """swap position x with position y"""
     x, y = int(words[0]), int(words[-1])
@@ -83,6 +72,5 @@ def descramble(data, scrambled):
             return "".join(original)
 
 
-assert scramble(test_data, "abcde") == "decab"
 print(scramble(data, "abcdefgh"))
 print(descramble(data, "fbgdceah"))
