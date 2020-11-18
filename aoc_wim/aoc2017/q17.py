f03930170a1abf5357d91abdbce5bbd354134dc2
wim glenn <hey@wimglenn.com>
2020-11-18 01:45:29 -0600

    HUGE speedup for 2017 day 17

--- aoc_wim/aoc2017/q17.py
+++ aoc_wim/aoc2017/q17.py
@@ -4,26 +4,25 @@ https://adventofcode.com/2017/day/17
 """
 from aocd import data
 
+n = int(data)
 
-def part_a(n):
-    L = [0]
-    pos = 0
-    for i in range(1, 2017 + 1):
-        pos = 1 + (pos + n) % i
-        L.insert(pos, i)
-    return L[pos + 1]
+L = [0]
+pos = 0
+for i in range(1, 2017 + 1):
+    pos = 1 + (pos + n) % i
+    L.insert(pos, i)
+print("part a:", L[pos + 1])
 
-
-def part_b(n):
-    pos = 0
-    i1 = 1
-    for i in range(1, 50000000 + 1):
-        pos = 1 + (pos + n) % i
-        if pos == 1:
-            i1 = i
-    return i1
-
-
-assert part_a(3) == 638
-print("part a:", part_a(int(data)))
-print("part b:", part_b(int(data)))
+pos = 0
+i = b = 1
+while i <= 50000000:
+    pos = 1 + (pos + n) % i
+    if pos == 1:
+        b = i
+    # when pos is much smaller than i, we can skip
+    # ahead iterations. as i gets bigger, this
+    # becomes an increasingly effective speedup
+    inci = (i - pos) // (n + 1) or 1
+    pos += (n + 1) * (inci - 1)
+    i += inci
+print("part b:", b)
