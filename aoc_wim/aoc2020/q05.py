88d8ac8d497ff09629cd687cfb88f3b3454ffd38
wim glenn <hey@wimglenn.com>
2020-12-05 01:57:17 -0600

    day 5

---
+++ aoc_wim/aoc2020/q05.py
@@ -0,0 +1,11 @@
+"""
+--- Day 5: Binary Boarding ---
+https://adventofcode.com/2020/day/5
+"""
+from aocd import data
+
+tr = str.maketrans("FBLR", "0101")
+ids = {int(x.translate(tr), 2) for x in data.splitlines()}
+print(max(ids))
+missing = set(range(min(ids), max(ids))) - set(ids)
+print(*missing)
