b753d9d53c184baedb707b65c0a0f4d3ea9683e9
wim glenn <hey@wimglenn.com>
2020-12-05 20:52:10 -0600

    remove redundant set call

--- aoc_wim/aoc2020/q05.py
+++ aoc_wim/aoc2020/q05.py
@@ -7,5 +7,5 @@ from aocd import data
 tr = str.maketrans("FBLR", "0101")
 ids = {int(x.translate(tr), 2) for x in data.splitlines()}
 print(max(ids))
-missing = set(range(min(ids), max(ids))) - set(ids)
+missing = set(range(min(ids), max(ids))) - ids
 print(*missing)
