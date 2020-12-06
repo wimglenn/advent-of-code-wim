918a7ce02c92f1a883f9a9f35b7b5350e91a7804
wim glenn <hey@wimglenn.com>
2020-12-05 23:44:31 -0600

    day 6

--- aoc_wim/aoc2020/q06.py
+++ aoc_wim/aoc2020/q06.py
@@ -4,10 +4,9 @@ https://adventofcode.com/2020/day/6
 """
 from aocd import data
 
-a = 0
-b = 0
+a = b = 0
 for block in data.split("\n\n"):
-    s = set(block) - {"\n"}
+    s = {*block} - {"\n"}
     a += len(s)
     b += len(s.intersection(*map(set, block.splitlines())))
 
