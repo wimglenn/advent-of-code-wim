abfa854c16cb048bbdf40c4d45502bece53415e5
wim glenn <hey@wimglenn.com>
2020-12-08 00:50:50 -0600

    day 8

---
+++ aoc_wim/aoc2020/__init__.py
@@ -0,0 +1,30 @@
+import logging
+
+
+log = logging.getLogger(__name__)
+
+
+class Comp:
+
+    def __init__(self, ops):
+        self.ops = ops  # parsed code
+        self.a = 0      # accumulator
+        self.line = 0   # instruction pointer into ops
+        self.n = 0      # number of instructions exec
+
+    def acc(self, a):
+        self.a += a
+
+    def jmp(self, i):
+        self.line += i - 1
+
+    def nop(self, _):
+        pass
+
+    def step(self):
+        self.n += 1
+        op, arg = self.ops[self.line]
+        op_func = getattr(self, op)
+        log.debug(f"{self.n:5}:   {op:3}{arg:4}   L={self.line:3}   a={self.a:4}")
+        op_func(arg)
+        self.line += 1
---
+++ aoc_wim/aoc2020/q08.py
@@ -0,0 +1,41 @@
+"""
+--- Day 8: Handheld Halting ---
+https://adventofcode.com/2020/day/8
+"""
+import logging
+from aocd import data
+from aoc_wim.aoc2020 import Comp
+
+# logging.basicConfig(format="%(message)s", level=logging.DEBUG)
+
+ops = []
+for line in data.splitlines():
+    op, arg = line.split()
+    ops.append((op, int(arg)))
+
+comp = Comp(ops)
+seen = {comp.line: comp.a}
+while True:
+    comp.step()
+    if comp.line in seen:
+        break
+    seen[comp.line] = comp.a
+print("part a:", comp.a)
+
+comps = []
+flipper = {"nop": "jmp", "jmp": "nop"}
+for i, (op, arg) in enumerate(ops):
+    if op in flipper:
+        c_ops = ops[:]
+        c_ops[i] = (flipper[op], arg)
+        comp = Comp(c_ops)
+        comps.append(comp)
+
+done = False
+while not done:
+    for comp in comps:
+        comp.step()
+        if comp.line == len(ops):
+            done = True
+            break
+print("part b:", comp.a)
