3005e9c3da3d29ab530734c3f970a9fe130ca8c4
wim glenn <hey@wimglenn.com>
2020-12-12 00:09:45 -0600

    day 12

--- aoc_wim/aoc2020/q12.py
+++ aoc_wim/aoc2020/q12.py
@@ -6,28 +6,26 @@ from aocd import data
 from aoc_wim.zgrid import ZGrid
 from aoc_wim.zgrid import manhattan_distance
 
-za = zb = 0
-waypoint = 10 - 1j
-dz = ZGrid.east
+za = zb = 0      # ferry
+zw = 10 - 1j     # waypoint
+dz = ZGrid.east  # direction
 
 for line in data.splitlines():
-    a = line[0]
+    i = line[0]
     n = int(line[1:])
-    if a in "NSEW":
-        za += n * getattr(ZGrid, a)
-        waypoint += n * getattr(ZGrid, a)
-    elif a in 'LR':
+    if i in "NSEW":
+        za += n * getattr(ZGrid, i)
+        zw += n * getattr(ZGrid, i)
+    elif i in "LR":
         n //= 90
-        turn = getattr(ZGrid, f"turn{a}")
+        turn = getattr(ZGrid, f"turn{i}")
         dz *= turn ** n
-        dw = waypoint - zb
-        dw *= turn ** n
-        waypoint = zb + dw
-    else:
+        zw = zb + (zw - zb) * turn ** n
+    elif i == "F":
         za += n * dz
-        delta = n * (waypoint - zb)
+        delta = n * (zw - zb)
         zb += delta
-        waypoint += delta
+        zw += delta
 
 print("part a:", manhattan_distance(za))
 print("part b:", manhattan_distance(zb))
