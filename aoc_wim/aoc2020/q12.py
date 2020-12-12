7c45c96783e076bf6a60d3efa5f31aa129f30b9e
wim glenn <hey@wimglenn.com>
2020-12-12 01:28:18 -0600

    use velocity

--- aoc_wim/aoc2020/q12.py
+++ aoc_wim/aoc2020/q12.py
@@ -6,26 +6,24 @@ from aocd import data
 from aoc_wim.zgrid import ZGrid
 from aoc_wim.zgrid import manhattan_distance
 
-za = zb = 0      # ferry
-zw = 10 - 1j     # waypoint
-dz = ZGrid.east  # direction
+za = zb = 0                          # ferry
+dza = ZGrid.east                     # direction (part a)
+dzb = 10 * ZGrid.east + ZGrid.north  # velocity (part b)
 
 for line in data.splitlines():
     i = line[0]
     n = int(line[1:])
     if i in "NSEW":
         za += n * getattr(ZGrid, i)
-        zw += n * getattr(ZGrid, i)
+        dzb += n * getattr(ZGrid, i)
     elif i in "LR":
         n //= 90
         turn = getattr(ZGrid, f"turn{i}")
-        dz *= turn ** n
-        zw = zb + (zw - zb) * turn ** n
+        dza *= turn ** n
+        dzb *= turn ** n
     elif i == "F":
-        za += n * dz
-        delta = n * (zw - zb)
-        zb += delta
-        zw += delta
+        za += n * dza
+        zb += n * dzb
 
 print("part a:", manhattan_distance(za))
 print("part b:", manhattan_distance(zb))
