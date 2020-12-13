948f6e871bf70ca45e64a94019326fec57e5ae13
wim glenn <hey@wimglenn.com>
2020-12-13 16:56:17 -0600

    day 13 without CRT

---
+++ aoc_wim/aoc2020/q13_sync.py
@@ -0,0 +1,19 @@
+from aocd import data
+
+t0, table = data.splitlines()
+t0 = int(t0)
+buses = [(i, int(b)) for i, b in enumerate(table.split(",")) if b != "x"]
+wait, bus = min([(-t0 % b, b) for _, b in buses])
+print("part a:", wait * bus)
+
+timestamp = 0
+increment = 1
+synced = {}
+while len(synced) < len(buses):
+    timestamp += increment
+    for phase, bus in buses:
+        if bus not in synced:
+            if (timestamp + phase) % bus == 0:
+                increment *= bus
+                synced[bus] = timestamp
+print("part b:", timestamp)
