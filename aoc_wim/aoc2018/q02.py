598ef362898a9809f18e28aafc4e132bf6d27453
wim glenn <hey@wimglenn.com>
2020-12-12 22:57:49 -0600

    2018/02

--- aoc_wim/aoc2018/q02.py
+++ aoc_wim/aoc2018/q02.py
@@ -8,43 +8,13 @@ from itertools import combinations
 from aocd import data
 
 
-def part_a(data):
-    counters = [Counter(s) for s in data.split()]
-    doubles = sum(1 for c in counters if 2 in c.values())
-    triples = sum(1 for c in counters if 3 in c.values())
-    return doubles * triples
+counters = [Counter(s) for s in data.split()]
+doubles = sum(1 for c in counters if 2 in c.values())
+triples = sum(1 for c in counters if 3 in c.values())
+print("part a:", doubles * triples)
 
-
-def part_b(data):
-    for a, b in combinations(data.split(), 2):
-        s = "".join([x for x, y in zip(a, b) if x == y])
-        if len(s) == len(a) - 1:
-            return s
-
-
-test_a = """
-abcdef
-bababc
-abbcde
-abcccd
-aabcdd
-abcdee
-ababab
-"""
-
-test_b = """
-abcde
-fghij
-klmno
-pqrst
-fguij
-axcye
-wvxyz
-"""
-
-assert part_a(test_a) == 12
-assert part_b(test_b) == "fgij"
-
-
-print("part a:", part_a(data))
-print("part b:", part_b(data))
+for a, b in combinations(data.split(), 2):
+    s = "".join([x for x, y in zip(a, b) if x == y])
+    if len(s) == len(a) - 1:
+        print("part b:", s)
+        break
