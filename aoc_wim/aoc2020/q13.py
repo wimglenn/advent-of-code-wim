217c448fcc59f845da2f61cb845d3cfab3bddbdf
wim glenn <hey@wimglenn.com>
2020-12-13 03:23:00 -0600

    get rid of the lambda

--- aoc_wim/aoc2020/q13.py
+++ aoc_wim/aoc2020/q13.py
@@ -5,15 +5,16 @@ https://adventofcode.com/2020/day/13
 import math
 from aocd import data
 
-bus, table = data.splitlines()
-bus = int(bus)
+t0, table = data.splitlines()
+t0 = int(t0)
 table = {-i: int(m) for i, m in enumerate(table.split(",")) if m != "x"}
-earliest = min(table.values(), key=lambda v: -bus % v)
-print("part a:", earliest * (-bus % earliest))
+bs, buses = zip(*table.items())
+wait, bus = min([(-t0 % b, b) for b in buses])
+print("part a:", wait * bus)
 
 # chinese remainder theorem...
-M = math.lcm(*table.values())
-ns = [M // m for m in table.values()]
-nis = [pow(n, -1, m) for n, m in zip(ns, table.values())]
-t = sum(b*n*ni for b, n, ni in zip(table, ns, nis)) % M
+M = math.lcm(*buses)
+ns = [M // m for m in buses]
+nis = [pow(n, -1, m) for n, m in zip(ns, buses)]
+t = sum(b*n*ni for b, n, ni in zip(bs, ns, nis)) % M
 print("part b:", t)
