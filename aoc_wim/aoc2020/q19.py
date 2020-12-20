64e075eaefcc81f552d8fb53dd2f7c564ff7c1e7
wim glenn <hey@wimglenn.com>
2020-12-20 02:55:28 -0600

    day 19

--- aoc_wim/aoc2020/q19.py
+++ aoc_wim/aoc2020/q19.py
@@ -10,7 +10,6 @@ data = data.replace('"', '')
 rules_raw, messages = data.split("\n\n")
 messages = messages.splitlines()
 rules = dict(line.split(": ") for line in rules_raw.splitlines())
-rr = {k: v for k, v in sorted(rules.items(), key=lambda v: int(v[0]))}
 digits = set("0123456789")
 
 
@@ -22,38 +21,32 @@ def eliminate(rules, k0):
         rules[k] = " ".join([v0 if k == k0 else k for k in v.split()])
 
 
-def rule0(rules):
+def simplify(rules):
     rules = rules.copy()
     while True:
         for k, v in rules.items():
             if not digits.intersection(v):
                 break
         else:
-            raise Exception(rules)
+            return rules
         eliminate(rules, k)
         if len(rules) == 1:
             break
-    return re.compile("^" + rules["0"].replace(" ", "") + "$")
+    return rules
 
 
-r0 = rule0(rules)
-a = sum(r0.match(m) is not None for m in messages)
-print("part a:", a)
+rules_a = simplify(rules)
+rule0_a = re.compile("^" + rules_a["0"].replace(" ", "") + "$")
+print("part a:", sum(rule0_a.match(m) is not None for m in messages))
 
 if {"8", "11"} < rules.keys():
     rules["8"] += " | 42 8"
     rules["11"] += " | 42 11 31"
-    try:
-        r0 = rule0(rules)
-    except Exception as err:
-        [rules] = err.args
+    rules = simplify(rules)
     r0 = rules.pop("0")
-    assert r0 == "8 11"
     r8 = rules.pop("8")
     r11 = rules.pop("11")
-    assert not rules
     r8 = r8.replace(" ", "")
-    assert r8.endswith("8")
     r8 = r8[:-1]
     r8 = r8[:len(r8)//2]
     r8 = r8.replace("(", "(?:")
@@ -64,6 +57,5 @@ if {"8", "11"} < rules.keys():
     pre = pre.replace("(", "(?:")
     post = post.replace("(", "(?:")
     r11 = "(" + pre + "(?2)?" + post + ")"
-    r0 = re.compile("^" + r8 + r11 + "$")
-    b = sum(r0.match(m) is not None for m in messages)
-    print("part b:", b)
+    rule0_b = re.compile("^" + r8 + r11 + "$")
+    print("part b:", sum(rule0_b.match(m) is not None for m in messages))
