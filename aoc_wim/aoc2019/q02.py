bbcd4b22c1dddfb66f4b860d9503e1c8ee7c9448
wim glenn <hey@wimglenn.com>
2021-01-14 22:54:32 -0600

    2019 tests

--- aoc_wim/aoc2019/q02.py
+++ aoc_wim/aoc2019/q02.py
@@ -25,16 +25,5 @@ def part_b(data):
                 return 100 * r1 + r2
 
 
-tests = {
-    "1,9,10,3,2,3,11,0,99,30,40,50": 3500,
-    "1,0,0,0,99": 2,
-    "2,3,0,3,99": 2,
-    "2,4,4,5,99,0": 2,
-    "1,1,1,4,99,5,6,0,99": 30,
-}
-
-for code, reg0 in tests.items():
-    assert part_a(code) == reg0
-
 print(part_a(data, r1=12, r2=2))
 print(part_b(data))
---
+++ tests/2019/test_02_1202_program_alarm.py
@@ -0,0 +1,15 @@
+import pytest
+from aoc_wim.aoc2019 import IntComputer
+
+
+@pytest.mark.parametrize("data,output", [
+    ("1,9,10,3,2,3,11,0,99,30,40,50", "3500,9,10,70,2,3,11,0,99,30,40,50"),
+    ("1,0,0,0,99", "2,0,0,0,99"),
+    ("2,3,0,3,99", "2,3,0,6,99"),
+    ("2,4,4,5,99,0", "2,4,4,5,99,9801"),
+    ("1,1,1,4,99,5,6,0,99", "30,1,1,4,2,5,6,0,99"),
+])
+def test_intcode_samples(data, output):
+    comp = IntComputer(data)
+    comp.run()
+    assert ",".join([str(v) for v in comp.reg.values()]) == output
