10c290ce188710e29ac8b0c484518119f6ca66e1
wim glenn <hey@wimglenn.com>
2021-01-14 22:38:19 -0600

    2019 tests

--- aoc_wim/aoc2019/q01.py
+++ aoc_wim/aoc2019/q01.py
@@ -3,30 +3,14 @@
 https://adventofcode.com/2019/day/1
 """
 from aocd import data
-
 import numpy as np
 
 
-def fuel(data, part="a"):
-    A = np.fromstring(data, dtype=int, sep="\n")
-    total = 0
-    while A.any():
-        A = (A // 3 - 2).clip(0)
-        total += A.sum()
-        if part == "a":
-            break
-    return total
-
-
-test_data = """
-12
-14
-1969
-100756
-"""
-
-assert fuel(test_data, part="a") == sum([2, 2, 654, 33583])
-assert fuel(test_data, part="b") == sum([2, 2, 966, 50346])
-
-print(fuel(data, part="a"))
-print(fuel(data, part="b"))
+A = np.fromstring(data, dtype=int, sep="\n")
+total = 0
+while A.any():
+    A = (A // 3 - 2).clip(0)
+    if not total:
+        print("part a:", A.sum())
+    total += A.sum()
+print("part b:", total)
