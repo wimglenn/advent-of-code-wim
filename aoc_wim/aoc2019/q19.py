9d65b1da1b737d711b72e110d2370113da9f168c
wim glenn <hey@wimglenn.com>
2021-01-18 19:43:54 -0600

    2019 tests

--- aoc_wim/aoc2019/q19.py
+++ aoc_wim/aoc2019/q19.py
@@ -17,28 +17,7 @@ def beam(z):
     return result
 
 
-print("populating 50x50 zgrid...")
-grid = ZGrid()
-x0 = 0
-for y in range(50):
-    on = False
-    for x in range(x0, 50):
-        z = x + y*1j
-        val = grid[z] = beam(z)
-        if not on and val:
-            on = True
-            x0 = x
-            if x0:
-                m = y / x0
-        if on and not val:
-            break
-
-print("part a", sum(grid.values()))
-grid.translate({0: ".", 1: "#"})
-grid.draw()
-
-
-def left_edge_of_beam(y, gradient):
+def left_edge_of_beam(y, gradient, beam=beam):
     x = int(y / gradient)
     z = x + y*1j
     if beam(z):
@@ -52,23 +31,48 @@ def left_edge_of_beam(y, gradient):
     return z
 
 
-print("initial gradient is approx -->", m)
-print("refining gradient estimate -->", end=" ")
-z = left_edge_of_beam(2000, gradient=m)
-m = z.imag/z.real
-print(m)
+def locate_square(beam, width, gradient_estimate=1., hi=None):
+    d = width - 1
+
+    def check(y):
+        z = left_edge_of_beam(y, gradient_estimate, beam)
+        val = beam(z + d * ZGrid.NE)
+        print(f"y={y}", "wide" if val else "narrow")
+        return val
+
+    bisect = Bisect(check, lo=d, hi=hi)
+    print("bisecting...")
+    y = bisect.run() + 1
+    z = left_edge_of_beam(y, gradient_estimate, beam) + d * ZGrid.N
+    return z
 
 
-def check(y, gradient=m):
-    z = left_edge_of_beam(y, gradient)
-    val = beam(z + d - d * 1j)
-    print(f"y={y}", "wide" if val else "narrow")
-    return val
+if __name__ == "__main__":
+    print("populating 50x50 zgrid...")
+    grid = ZGrid()
+    x0 = 0
+    for y in range(50):
+        on = False
+        for x in range(x0, 50):
+            z = x + y * 1j
+            val = grid[z] = beam(z)
+            if not on and val:
+                on = True
+                x0 = x
+                if x0:
+                    m = y / x0
+            if on and not val:
+                break
 
+    print("part a", sum(grid.values()))
+    grid.translate({0: ".", 1: "#"})
+    grid.draw()
 
-d = 99
-bisect = Bisect(check, lo=d)
-print("bisecting...")
-y = bisect.run() + 1
-z = left_edge_of_beam(y, m) - d * 1j
-print("part b", int(z.real)*10000 + int(z.imag))
+    print("initial gradient is approx -->", m)
+    print("refining gradient estimate -->", end=" ")
+    z = left_edge_of_beam(2000, gradient=m)
+    m = z.imag/z.real
+    print(m)
+
+    z = locate_square(beam, width=100, gradient_estimate=m)
+    print("part b", int(z.real)*10000 + int(z.imag))
