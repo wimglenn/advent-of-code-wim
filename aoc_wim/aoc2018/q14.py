bb5b2e0e149f377bc87b985066b8ea84c6355266
wim glenn <hey@wimglenn.com>
2021-01-23 02:52:40 -0600

    faster 2018/14

--- aoc_wim/aoc2018/q14.py
+++ aoc_wim/aoc2018/q14.py
@@ -14,15 +14,23 @@ class RecipeList:
         self.elf2 = 1
 
     def grow(self, min_length):
-        lookup = {n: [int(x) for x in str(n)] for n in range(19)}
-        prev_len = len(self.scores)
-        while len(self.scores) < min_length:
-            score1 = self.scores[self.elf1]
-            score2 = self.scores[self.elf2]
-            self.scores.extend(lookup[score1 + score2])
-            self.elf1 = (self.elf1 + 1 + score1) % len(self.scores)
-            self.elf2 = (self.elf2 + 1 + score2) % len(self.scores)
-        self.scores_str += "".join([str(x) for x in self.scores[prev_len:]])
+        scores = self.scores
+        e1, e2 = self.elf1, self.elf2
+        prev_len = len(scores)
+        while len(scores) < min_length:
+            score1 = scores[e1]
+            score2 = scores[e2]
+            score = score1 + score2
+            if score >= 10:
+                scores.append(1)
+                scores.append(score - 10)
+            else:
+                scores.append(score)
+            e1 = (e1 + 1 + score1) % len(scores)
+            e2 = (e2 + 1 + score2) % len(scores)
+        self.elf1 = e1
+        self.elf2 = e2
+        self.scores_str += "".join([str(x) for x in scores[prev_len:]])
 
 
 recipes = RecipeList()
