ab011ac8867239cff300f5a848c896982ef4446f
wim glenn <hey@wimglenn.com>
2021-01-22 22:56:44 -0600

    clever parse

--- aoc_wim/aoc2017/q13.py
+++ aoc_wim/aoc2017/q13.py
@@ -5,17 +5,10 @@ https://adventofcode.com/2017/day/13
 from aocd import data
 
 
-depth2range = {}
-for line in data.splitlines():
-    d, r = line.split(": ")
-    depth2range[int(d)] = int(r)
-
+depth2range = dict(map(int, line.split(":")) for line in data.splitlines())
 print("part a:", sum(d * r for d, r in depth2range.items() if d % (2 * r - 2) == 0))
 
 t = 0
-while True:
-    if all((d + t) % (2 * r - 2) for d, r in depth2range.items()):
-        break
+while not all((d + t) % (2 * r - 2) for d, r in depth2range.items()):
     t += 1
-
 print("part b:", t)
