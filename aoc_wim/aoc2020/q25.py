256bf0e52c24b0a1fd86cf8cfaf64e9e1cdb6887
wim glenn <hey@wimglenn.com>
2021-01-23 02:58:37 -0600

    faster 2020/25

--- aoc_wim/aoc2020/q25.py
+++ aoc_wim/aoc2020/q25.py
@@ -4,17 +4,17 @@ https://adventofcode.com/2020/day/25
 """
 from aocd import data
 
-pk_card, pk_door = [int(line) for line in data.split()]
-subject_number = 7
-modulus = 20201227
 
-loop_size = 0
-while True:
-    loop_size += 1
-    p = pow(subject_number, loop_size, modulus)
-    if p == pk_card:
-        print(pow(pk_door, loop_size, modulus))
-        break
-    if p == pk_door:
-        print(pow(pk_card, loop_size, modulus))
-        break
+def crack(pk_card, pk_door, subject_number=7, modulus=20201227):
+    loop_size = 0
+    while True:
+        loop_size += 1
+        p = pow(subject_number, loop_size, modulus)
+        if p == pk_card:
+            return pow(pk_door, loop_size, modulus)
+        if p == pk_door:
+            return pow(pk_card, loop_size, modulus)
+
+
+pk_card, pk_door = data.split()
+print(crack(int(pk_card), int(pk_door)))
