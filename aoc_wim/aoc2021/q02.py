cd4f386c42086b391739a9cacbb032926b8977d9
wim glenn <wim.glenn@gmail.com>
2021-12-01 23:56:22 -0600

    day 2 (#19)
    * day 2
    
    * dive

---
+++ aoc_wim/aoc2021/q02.py
@@ -0,0 +1,20 @@
+"""
+--- Day 2: Dive! ---
+https://adventofcode.com/2021/day/2
+"""
+from aocd import data
+from aoc_wim.zgrid import ZGrid
+
+aim = za = zb = 0
+for line in data.splitlines():
+    direction, X = line.split()
+    X = int(X)
+    dz = ZGrid.dzs[direction]
+    za += dz * X
+    if direction == "forward":
+        zb += X + aim * X
+    else:
+        aim += dz * X
+
+print("part a:", int(za.real) * int(za.imag))
+print("part b:", int(zb.real) * int(zb.imag))
