9902b1608cc1c971fc881bb8b40360bea5f6e1cd
wim glenn <hey@wimglenn.com>
2021-12-09 18:50:51 -0600

    shorter day 9

--- aoc_wim/aoc2021/q09.py
+++ aoc_wim/aoc2021/q09.py
@@ -5,10 +5,8 @@ https://adventofcode.com/2021/day/9
 from aocd import data
 import networkx as nx
 from aoc_wim.zgrid import ZGrid
-import heapq
-import math
 
 g = ZGrid(data)
-print("part a:", sum(1 + int(n) for z0, n in g.items() if all(n < g.get(z, 'z') for z in g.near(z0))))
-basin_sizes = [len(b) for b in nx.connected_components(g.graph(extra="012345678"))]
-print("part b:", math.prod(heapq.nlargest(3, basin_sizes)))
+basins = sorted(nx.connected_components(g.graph(extra="012345678")), key=len)
+print("part a:", sum(min([1 + int(g[z]) for z in basin]) for basin in basins))
+print("part b:", len(basins[-1]) * len(basins[-2]) * len(basins[-3]))
