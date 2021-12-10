91443c6a9062ecddeb32c19eb802b8ccc2692940
wim glenn <hey@wimglenn.com>
2021-12-09 19:21:47 -0600

    shorter

--- aoc_wim/aoc2021/q09.py
+++ aoc_wim/aoc2021/q09.py
@@ -1,12 +1,12 @@
 """
---- Day 9: Smoke Basin ---
+--- Day 9: Smoke b ---
 https://adventofcode.com/2021/day/9
 """
 from aocd import data
 import networkx as nx
 from aoc_wim.zgrid import ZGrid
 
-grid = ZGrid(data)
-basins = sorted(nx.connected_components(grid.graph(extra="012345678")), key=len)
-print("part a:", sum(min([1 + int(grid[z]) for z in basin]) for basin in basins))
-print("part b:", len(basins[-1]) * len(basins[-2]) * len(basins[-3]))
+g = ZGrid(data)
+bs = sorted(nx.connected_components(g.graph(extra="012345678")), key=len)
+print("part a:", sum(min([1 + int(g[z]) for z in b]) for b in bs))
+print("part b:", len(bs[-1]) * len(bs[-2]) * len(bs[-3]))
