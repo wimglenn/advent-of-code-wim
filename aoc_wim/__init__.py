c0d16c5efdd6caed7bcfcc43e01bb262d9c81647
wim glenn <hey@wimglenn.com>
2021-12-12 00:27:08 -0600

    day 12

--- aoc_wim/__init__.py
+++ aoc_wim/__init__.py
@@ -4,7 +4,7 @@ import runpy
 import sys
 
 
-__version__ = "2021.11"
+__version__ = "2021.12"
 
 
 def plugin(year, day, data):
---
+++ aoc_wim/aoc2021/q12.py
@@ -0,0 +1,43 @@
+"""
+--- Day 12: Passage Pathing ---
+https://adventofcode.com/2021/day/12
+"""
+from aocd import data
+from collections import deque, defaultdict
+
+
+caves = defaultdict(set)
+for line in data.splitlines():
+    x, y = line.split("-")
+    caves[x].add(y)
+    caves[y].add(x)
+
+
+def get_neighbors_a(here, visited):
+    return [c for c in caves[here] if c.isupper() or c not in visited]
+
+
+def get_neighbors_b(here, visited):
+    if any(v > 1 for k, v in visited.items() if not k.isupper()):
+        # we've already visited a small cave twice
+        return get_neighbors_a(here, visited)
+    return [n for n in caves[here] if n != "start"]
+
+
+def n_paths(part):
+    get_neighbors = get_neighbors_a if part == "a" else get_neighbors_b
+    result = 0
+    q = deque([(["start"], {"start": 1})])
+    while q:
+        path, visited = q.pop()
+        here = path[-1]
+        if here == "end":
+            result += 1
+            continue
+        neighbors = get_neighbors(here, visited)
+        q.extend([(path + [n], {**visited, n: visited.get(n, 0) + 1}) for n in neighbors])
+    return result
+
+
+print("part a:", n_paths("a"))
+print("part b:", n_paths("b"))
