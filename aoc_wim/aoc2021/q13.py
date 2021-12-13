90adb8512cef1afecd6cfd96a382acb407f80ab1
wim glenn <hey@wimglenn.com>
2021-12-13 00:15:35 -0600

    literal eval

--- aoc_wim/aoc2021/q13.py
+++ aoc_wim/aoc2021/q13.py
@@ -4,12 +4,10 @@ https://adventofcode.com/2021/day/13
 """
 from aocd import data
 from aoc_wim.ocr import AOCR
+from ast import literal_eval
 
 points, folds = data.split("\n\n")
-d = {}
-for line in points.splitlines():
-    real, imag = line.split(",")
-    d[complex(int(real), int(imag))] = "#"
+d = {literal_eval(p.replace(",", "+") + "j"): "#" for p in points.split()}
 
 for i, fold in enumerate(folds.splitlines()):
     axis, n = fold.split("=")
