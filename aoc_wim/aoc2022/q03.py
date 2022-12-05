ac3d593b34026c75521dfb87094764d480637252
wim glenn <hey@wimglenn.com>
2022-12-04 20:48:51 -0600

    slightly more efficient

--- aoc_wim/aoc2022/q03.py
+++ aoc_wim/aoc2022/q03.py
@@ -2,7 +2,6 @@
 --- Day 3: Rucksack Reorganization ---
 https://adventofcode.com/2022/day/3
 """
-import string
 from aocd import data
 from wimpy import chunks
 
@@ -12,13 +11,13 @@ for line in data.splitlines():
     left, right = line[:i], line[i:]
     [c] = set(left).intersection(right)
     a += c.isupper() * 26
-    a += string.ascii_lowercase.index(c.lower()) + 1
+    a += ord(c.lower()) - ord("a") + 1
 
 b = 0
 for s1, s2, s3 in chunks(data.splitlines()):
     [c] = set(s1).intersection(s2, s3)
     b += c.isupper() * 26
-    b += string.ascii_lowercase.index(c.lower()) + 1
+    b += ord(c.lower()) - ord("a") + 1
 
 print("part a:", a)
 print("part b:", b)
