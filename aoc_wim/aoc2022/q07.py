670145d08548c0281bb758cbb5829d5952b5b012
wim glenn <hey@wimglenn.com>
2022-12-07 01:34:19 -0600

    less nesting

--- aoc_wim/aoc2022/q07.py
+++ aoc_wim/aoc2022/q07.py
@@ -3,30 +3,27 @@
 https://adventofcode.com/2022/day/7
 """
 from aocd import data
-from collections import deque
 
-
-q = deque(data.splitlines())
-assert q.popleft() == "$ cd /"
+lines = iter(data.splitlines())
+assert next(lines) == "$ cd /"
 cwd = "/"
 dirs = {"/": 0}  # map of dirs to their cumulative size
 parents = []  # cwd stack
-while q:
-    match q.popleft().split():
+for line in lines:
+    match line.split():
         case ["$", "cd", ".."]:
             cwd = parents.pop()
         case ["$", "cd", name]:
             parents.append(cwd)
             cwd += name + "/"
         case ["$", "ls"]:
-            while q and not q[0].startswith("$"):
-                match q.popleft().split():
-                    case ["dir", name]:
-                        dirs[cwd + name + "/"] = 0
-                    case [s, name]:
-                        size = int(s)
-                        for d in parents + [cwd]:
-                            dirs[d] += size
+            pass
+        case ["dir", name]:
+            dirs[cwd + name + "/"] = 0
+        case [s, name]:
+            size = int(s)
+            for d in parents + [cwd]:
+                dirs[d] += size
 
 total_space = 70_000_000
 df_target = 30_000_000
