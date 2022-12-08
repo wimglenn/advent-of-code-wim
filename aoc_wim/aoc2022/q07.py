9db83b298c7a976f2677a0133532058137eda5fc
wim glenn <hey@wimglenn.com>
2022-12-08 00:56:04 -0600

    simplify

--- aoc_wim/aoc2022/q07.py
+++ aoc_wim/aoc2022/q07.py
@@ -4,36 +4,21 @@ https://adventofcode.com/2022/day/7
 """
 from aocd import data
 from pathlib import Path
+from collections import Counter
 
 cwd = Path()
-dirs = {}
+dirs = Counter()
 
 for line in data.splitlines():
     match line.split():
-        case ["$", "cd", "/"]:
-            cwd = Path("/")
-            dirs[cwd] = 0
-        case ["$", "cd", ".."]:
-            cwd = cwd.parent
-        case ["$", "cd", name]:
-            cwd /= name
-        case ["$", "ls"]:
-            pass
-        case ["dir", name]:
-            dirs[cwd / name] = 0
-        case [s, name]:
-            size = int(s)
+        case ["$", "cd", name]: cwd = cwd.joinpath(name).resolve()
+        case [s, name] if s.isdigit():
             for p in [cwd, *cwd.parents]:
-                dirs[p] += size
+                dirs[p] += int(s)
 
-total_space = 70_000_000
-df_target = 30_000_000
-used_space = dirs[Path("/")]
-df_actual = total_space - used_space
-del_needed = df_target - df_actual
-
+rmbytes = dirs[Path("/")] - 70_000_000 + 30_000_000
 a = sum(v for v in dirs.values() if v <= 100_000)
-b = min(v for v in dirs.values() if v >= del_needed)
+b = min(v for v in dirs.values() if v >= rmbytes)
 
 print("part a:", a)
 print("part b:", b)
