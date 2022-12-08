c5c182233dec1e35c04097521ee5f8aebbd6f6b6
wim glenn <hey@wimglenn.com>
2022-12-08 00:21:24 -0600

    day 8

---
+++ aoc_wim/aoc2022/q08.py
@@ -0,0 +1,33 @@
+"""
+--- Day 8: Treetop Tree House ---
+https://adventofcode.com/2022/day/8
+"""
+from aocd import data
+from aoc_wim.zgrid import ZGrid, zrange
+
+grid = ZGrid(data, transform=int)
+h, w = grid.height, grid.width
+a = 2*h + 2*w - 4  # perimeter trees are always visible
+b = 0
+for z0 in zrange(complex(1, 1), complex(w - 1, h - 1)):  # interior trees
+    n = grid[z0]
+    vis = False  # visibility from exterior
+    ss = 1  # scenic score
+    for dz in 1, -1, 1j, -1j:
+        z = z0
+        s = 0
+        while True:
+            z += dz
+            s += 1
+            if z not in grid:
+                s -= 1
+                vis = True
+                break
+            if grid[z] >= n:
+                break
+        ss *= s
+    a += vis
+    b = max(b, ss)
+
+print("part a:", a)
+print("part b:", b)
