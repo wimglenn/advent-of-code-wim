0ad57abcbbd42874ac65f0410f3bcd8d2ddaddca
wim glenn <hey@wimglenn.com>
2022-12-09 00:34:07 -0600

    day 9

--- aoc_wim/aoc2022/q09.py
+++ aoc_wim/aoc2022/q09.py
@@ -9,25 +9,23 @@ def cmp(x, y):
     return (x > y) - (x < y)
 
 
-def T1(H, T0):
-    dz = H - T0
+def dT(H, T):
+    dz = H - T
     if abs(dz.imag) > 1 or abs(dz.real) > 1:
-        T0 += complex(cmp(dz.real, 0), cmp(dz.imag, 0))
-    return T0
+        T += cmp(dz.real, 0) + 1j * cmp(dz.imag, 0)
+    return T
 
 
 dHs = dict(zip("UDLR", (-1j, 1j, -1, 1)))
 for part in "ab":
-    L = 2 if part == "a" else 10
-    Ts = [0] * L
+    tail_length = 2 if part == "a" else 10
+    zs = [0] * tail_length
     seen = set()
     for line in data.splitlines():
         dH, n = line.split()
         for _ in range(int(n)):
-            Ts[0] += dHs[dH]
-            for i in range(1, L):
-                H = Ts[i-1]
-                T = Ts[i]
-                Ts[i] = T1(H, T)
-            seen.add(Ts[-1])
+            zs[0] += dHs[dH]
+            for i in range(1, tail_length):
+                zs[i] = dT(*zs[i - 1:i + 1])
+            seen.add(zs[-1])
     print(f"part {part}:", len(seen))
