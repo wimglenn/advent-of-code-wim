e5dd55e10ce2bfe80db2975dd2c7b74fec0d2a88
wim glenn <hey@wimglenn.com>
2022-12-13 02:47:23 -0600

    day 12

--- aoc_wim/aoc2022/q12.py
+++ aoc_wim/aoc2022/q12.py
@@ -15,7 +15,7 @@ grid[end] = "z"
 
 class AStarA(AStar):
     def adjacent(self, z0):
-        return [z for z in grid.near(z0) if ord(grid.get(z, "⛰")) - ord(grid[z0]) <= 1]
+        return [z for z in grid.near(z0) if ord(grid.get(z, "‾")) - ord(grid[z0]) <= 1]
 
 
 class AStarB(AStar):
@@ -23,8 +23,8 @@ class AStarB(AStar):
         return [z for z in grid.near(z0) if ord(grid.get(z, "_")) - ord(grid[z0]) >= -1]
 
     def target_reached(self, z, target):
-        return grid[z] == "a"
+        return grid[z] == target
 
 
 print("part a:", len(AStarA(start, end).run()) - 1)
-print("part b:", len(AStarB(end, None).run()) - 1)
+print("part b:", len(AStarB(end, "a").run()) - 1)
