a062a321195a23c1dea72a976b6a3b6081314a6d
wim glenn <hey@wimglenn.com>
2023-02-18 21:43:11 -0600

    faster day 16

--- aoc_wim/aoc2022/q16.py
+++ aoc_wim/aoc2022/q16.py
@@ -3,67 +3,83 @@
 https://adventofcode.com/2022/day/16
 """
 from aocd import data
+from collections import defaultdict
 from collections import deque
-import networkx as nx
 import itertools
-import heapq
+from parse import parse
 
 
-valves = {}
-graph = nx.Graph()
+flow = {}
+template = "Valve {} ha flow rate={:d}; tunnel lead to valve {}"
+dist = defaultdict(lambda: float("inf"))
 for line in data.splitlines():
-    parts = line.split()
-    f = int(parts[4].split("=")[1][:-1])
-    v, *vs = [x.rstrip(",") for x in parts if x.rstrip(",").isupper()]
-    if f > 0:
-        valves[v] = f
-    for t in vs:
-        graph.add_edge(v, t)
+    line = line.replace("s ", " ")
+    v1, f, v2s = parse(template, line).fixed
+    flow[v1] = f
+    dist[v1, v1] = 0
+    for v2 in v2s.split(", "):
+        dist[v1, v2] = dist[v2, v1] = 1
 
-dist = {}
-for p1, p2 in itertools.combinations(["AA", *valves], 2):
-    dist[p1, p2] = dist[p2, p1] = nx.shortest_path_length(graph, p1, p2)
+# https://en.wikipedia.org/wiki/Floyd-Warshall_algorithm
+for k, i, j in itertools.product(flow, repeat=3):
+    dist[i, j] = min(dist[i, j], dist[i, k] + dist[k, j])
+
+nonzero_flow = {k: v for k, v in flow.items() if v > 0}
 
 
-def go(dont_visit=(), max_visits=float("inf")):
+def go(max_t=30):
     # state vector:
-    # current minute (int), current position (str), valves opened: minute (dict)
+    # current minute (int), current position (str), {valves_opened: minute} (dict)
     s0 = 0, "AA", {}
     result = []
     q = deque([s0])
     while q:
-        m, pos, v = q.popleft()
-        if m >= M:
-            result.append(v)
-            continue
-        candidates = [p for p in valves if p not in v and p not in dont_visit]
-        if not candidates or len(v) >= max_visits:
-            result.append(v)
-            continue
-        for p in candidates:
+        t, pos, visited = q.popleft()
+        end = True
+        for p in nonzero_flow:
+            if p in visited:
+                continue
             d = dist[pos, p]
-            if m + d + 1 > M:
-                q.append((M, p, v))
-            else:
-                q.append((m + d + 1, p, {**v, p: m + d + 1}))
+            if t + d + 1 < max_t:
+                end = False
+                q.append((t + d + 1, p, {**visited, p: t + d + 1}))
+        if end:
+            result.append(visited)
     return result
 
 
-def score(end):
-    return sum((M - v) * valves[k] for k, v in end.items())
+def score(visited, max_t=30):
+    return sum((max_t - t) * flow[k] for k, t in visited.items())
 
 
-M = 30
 paths = go()
 print("part a:", max(score(p) for p in paths))
 
-M = 26
+scores = {}
+for path in paths:
+    # just prune the paths we already had for max_t=30
+    path = {k: v for k, v in path.items() if v < 26}
+    vs = frozenset(path)
+    s = score(path, max_t=26)
+    if scores.get(vs, -1) < s:
+        scores[vs] = s
+
+# order the paths with highest pressure relief first
+visited = sorted(scores, key=scores.get, reverse=True)
+pressures = [scores[k] for k in visited]
+
 b = 0
-your_paths = go(max_visits=len(valves) // 2)
-for your_path in heapq.nlargest(20, your_paths, key=score):
-    your_score = score(your_path)
-    ele_paths = go(dont_visit=your_path)
-    for ele_path in ele_paths:
-        ele_score = score(ele_path)
-        b = max(b, your_score + ele_score)
+for i in range(len(visited)):
+    v1 = visited[i]
+    p1 = pressures[i]
+    for j in range(i + 1, len(visited)):
+        v2 = visited[j]
+        p2 = pressures[j]
+        if not v1 & v2:
+            b = max(b, p1 + p2)
+            break
+    if p1 * 2 <= b:
+        # short-circuit because we can not possibly do any better now
+        break
+
 print("part b:", b)
