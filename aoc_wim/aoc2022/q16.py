63958af5b38999e39ee3da2a9b76be938f68a388
wim glenn <hey@wimglenn.com>
2023-02-18 22:52:02 -0600

    faster day 16

--- aoc_wim/aoc2022/q16.py
+++ aoc_wim/aoc2022/q16.py
@@ -27,58 +27,51 @@ for k, i, j in itertools.product(flow, repeat=3):
 nonzero_flow = {k: v for k, v in flow.items() if v > 0}
 
 
-def go(max_t=30):
+def find_paths():
     # state vector:
-    # current minute (int), current position (str), {valves_opened: minute} (dict)
-    s0 = 0, "AA", {}
-    result = []
+    # current minute (int), current score (int), current valve (str), {valves_opened: minute} (dict)
+    s0 = 0, 0, "AA", {}
+    result = {}
     q = deque([s0])
     while q:
-        t, pos, visited = q.popleft()
-        end = True
+        t, s, pos, visited = q.popleft()
+        k = frozenset(visited)
+        if k not in result or result[k][1] < s:
+            result[k] = visited, s
         for p in nonzero_flow:
             if p in visited:
                 continue
             d = dist[pos, p]
-            if t + d + 1 < max_t:
-                end = False
-                q.append((t + d + 1, p, {**visited, p: t + d + 1}))
-        if end:
-            result.append(visited)
-    return result
+            dt = 30 - t - d - 1
+            if dt > 0:
+                q.append((t + d + 1, s + flow[p] * dt, p, {**visited, p: t + d + 1}))
+    return result.values()
 
 
-def score(visited, max_t=30):
-    return sum((max_t - t) * flow[k] for k, t in visited.items())
-
-
-paths = go()
-print("part a:", max(score(p) for p in paths))
+paths = find_paths()
+print("part a:", max(s for v, s in paths))
 
 scores = {}
-for path in paths:
+for path, _old_score in paths:
     # just prune the paths we already had for max_t=30
     path = {k: v for k, v in path.items() if v < 26}
-    vs = frozenset(path)
-    s = score(path, max_t=26)
-    if scores.get(vs, -1) < s:
-        scores[vs] = s
+    k = frozenset(path)
+    # scores need to be recalculated
+    score = sum((26 - t) * flow[k] for k, t in path.items())
+    if scores.get(k, -1) < score:
+        scores[k] = score
 
 # order the paths with highest pressure relief first
 visited = sorted(scores, key=scores.get, reverse=True)
-pressures = [scores[k] for k in visited]
-
 b = 0
 for i in range(len(visited)):
     v1 = visited[i]
-    p1 = pressures[i]
     for j in range(i + 1, len(visited)):
         v2 = visited[j]
-        p2 = pressures[j]
         if not v1 & v2:
-            b = max(b, p1 + p2)
+            b = max(b, scores[v1] + scores[v2])
             break
-    if p1 * 2 <= b:
+    if scores[v1] * 2 <= b:
         # short-circuit because we can not possibly do any better now
         break
 
