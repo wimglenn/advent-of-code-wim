13957d76754135e3e8408edf7664920b01151a69
wim glenn <hey@wimglenn.com>
2023-03-01 00:23:36 -0600

    faster day 19

--- aoc_wim/aoc2022/q19.py
+++ aoc_wim/aoc2022/q19.py
@@ -3,141 +3,54 @@
 https://adventofcode.com/2022/day/19
 """
 from aocd import data
-from parse import parse
-from typing import NamedTuple
+import math
+import re
 
 
-template = (
-    "Blueprint {id:d}: "
-    "Each ore robot costs {ore_ore:d} ore. "
-    "Each clay robot costs {cla_ore:d} ore. "
-    "Each obsidian robot costs {obs_ore:d} ore and {obs_cla:d} clay. "
-    "Each geode robot costs {geo_ore:d} ore and {geo_obs:d} obsidian."
-)
-if len(data.split("\n\n")) == 2:
-    lines = data.replace("  ", " ").replace("\n\n", " --- ").replace("\n", "").split(" --- ")
-else:
-    lines = data.splitlines()
-
-
-class Node(NamedTuple):
-    n_geo: int = 0
-    n_obs: int = 0
-    n_cla: int = 0
-    n_ore: int = 0
-
-    r_geo: int = 0
-    r_obs: int = 0
-    r_cla: int = 0
-    r_ore: int = 1
-
-    def buy(self, rtype=None):
-        n_geo = self.n_geo + self.r_geo
-        n_obs = self.n_obs + self.r_obs
-        n_cla = self.n_cla + self.r_cla
-        n_ore = self.n_ore + self.r_ore
-        r_geo = self.r_geo
-        r_obs = self.r_obs
-        r_cla = self.r_cla
-        r_ore = self.r_ore
-        if rtype == "geo":
-            r_geo += 1
-        elif rtype == "obs":
-            r_obs += 1
-        elif rtype == "cla":
-            r_cla += 1
-        elif rtype == "ore":
-            r_ore += 1
-        if rtype is not None:
-            cost_obs, cost_cla, cost_ore = bp[f"cost_{rtype}"]
-            n_obs -= cost_obs
-            n_cla -= cost_cla
-            n_ore -= cost_ore
-        return Node(n_geo, n_obs, n_cla, n_ore, r_geo, r_obs, r_cla, r_ore)
-
-
-blueprints = []
-rtypes = "ore", "cla", "obs", "geo"
+lines = data.split("\n\n") if "\n\n" in data else data.splitlines()
+bps = []
 for line in lines:
-    parsed = parse(template, line).named
-    bp = {"pk": parsed["id"]}
-    costs = []
-    for r0 in rtypes:
-        k = f"cost_{r0}"
-        cost = parsed.get(f"{r0}_obs", 0), parsed.get(f"{r0}_cla", 0), parsed.get(f"{r0}_ore", 0)
-        bp[k] = cost
-        costs.append(cost)
-    bp["max_ore"] = max(c[2] for c in costs)
-    bp["max_cla"] = max(c[1] for c in costs)
-    bp["max_obs"] = max(c[0] for c in costs)
-    blueprints.append(bp)
+    pk, ore3, ore2, ore1, clay, ore0, obs = map(int, re.findall(r"\d+", line))
+    bps.append((pk, ore3, ore2, ore1, clay, ore0, obs, max(ore3, ore2, ore1, ore0)))
 
 
-def adj(n0):
-    result = []
-    c_obs, c_cla, c_ore = bp["cost_geo"]
-    if c_obs <= n0.n_obs and c_ore <= n0.n_ore:
-        result.append(n0.buy(rtype="geo"))
-
-    c_obs, c_cla, c_ore = bp["cost_obs"]
-    if c_cla <= n0.n_cla and c_ore <= n0.n_ore and n0.r_geo <= 2 and n0.r_obs < bp["max_obs"]:
-        result.append(n0.buy(rtype="obs"))
-
-    c_obs, c_cla, c_ore = bp["cost_cla"]
-    if c_ore <= n0.n_ore and n0.r_obs <= 2 and n0.r_geo == 0 and n0.r_cla < bp["max_cla"]:
-        result.append(n0.buy(rtype="cla"))
-
-    c_obs, c_cla, c_ore = bp["cost_ore"]
-    if c_ore <= n0.n_ore and n0.r_cla <= 2 and n0.r_obs == 0 and n0.r_geo == 0 and n0.r_ore < bp["max_ore"]:
-        result.append(n0.buy(rtype="ore"))
-
-    result.append(n0.buy())  # buy nothing - increases budget and elapsed time
-    return result
-
-
-class DFS:
-
-    def __init__(self, adj, max_depth=None):
-        self.adj = adj
-        self.seen = {}
-        self.max_depth = max_depth
-
-    def __call__(self, s0, target=None):
-        stack = [(0, s0)]
-        maxd = self.max_depth or float("inf")
-        seen = self.seen
-        best = 0
-        while stack:
-            depth, state = stack.pop()
-            if state in seen and seen[state] <= depth:
-                # already got here by a more efficient path - don't bother extending
-                continue
-            seen[state] = depth
-            if state.n_geo > best:
-                best = state.n_geo
-                print(f"new best {best} - {len(seen)} {len(stack)} depth={depth}, {state}")
-            if depth + 1 <= maxd:
-                t_remain = maxd - depth
-                potential = state.n_geo + t_remain * state.r_geo + (t_remain * (t_remain - 1))//2
-                if potential >= best:
-                    stack += [(depth + 1, s) for s in self.adj(state)[::-1]]
-        return best
+def search(stack, blueprint):
+    _pk, ore3, ore2, ore1, clay, ore0, obs, ore = blueprint
+    tri = [t * (t - 1) // 2 for t in range(33)]
+    max_geodes = 0
+    while stack:
+        r, n0, n1, n2, n3, r0, r1, r2, r3, t = stack.pop()
+        g = n0 + r0 * t
+        if g > max_geodes:
+            max_geodes = g
+        if t == 1:
+            continue  # on the very last minute, building another robot is not useful
+        if r == 0 and not r1:
+            continue  # you need at least one obsidian robot before considering a geode robot
+        elif r == 1 and not r2:
+            continue  # you need at least one clay robot before considering an obsidian robot
+        if r == 1 and r1 >= obs:
+            continue  # don't need any more obsidian than you can actually spend
+        elif r == 2 and r2 >= clay:
+            continue  # don't need any more clay than you can actually spend
+        elif r == 3 and r3 >= ore:
+            continue  # don't need any more ore than you can actually spend
+        if n0 + t * r0 + tri[t] <= max_geodes:
+            continue  # prune this branch - it can't beat our best so far
+        if r == 0 and n1 >= obs and n3 >= ore0:  # get a geode robot
+            stack += [(r, n0 + r0, n1 + r1 - obs, n2 + r2, n3 + r3 - ore0, r0 + 1, r1, r2, r3, t - 1) for r in range(4)]
+        elif r == 1 and n2 >= clay and n3 >= ore1:  # get an obsidian robot
+            stack += [(r, n0 + r0, n1 + r1, n2 + r2 - clay, n3 + r3 - ore1, r0, r1 + 1, r2, r3, t - 1) for r in range(4)]
+        elif r == 2 and n3 >= ore2:  # get a clay robot
+            stack += [(r, n0 + r0, n1 + r1, n2 + r2, n3 + r3 - ore2, r0, r1, r2 + 1, r3, t - 1) for r in range(4)]
+        elif r == 3 and n3 >= ore3:  # get an ore robot
+            stack += [(r, n0 + r0, n1 + r1, n2 + r2, n3 + r3 - ore3, r0, r1, r2, r3 + 1, t - 1) for r in range(4)]
+        else:  # just wait - accumulates more minerals
+            stack.append((r, n0 + r0, n1 + r1, n2 + r2, n3 + r3, r0, r1, r2, r3, t - 1))
+    print(f"{max_geodes=} {blueprint=}")
+    return max_geodes
 
 
-a = 0
-for bp in blueprints:
-    bfs = DFS(adj, max_depth=24)
-    bfs(Node())
-    best = max([s.n_geo for s, d in bfs.seen.items() if d == 24])
-    print(f"n_geodes={best} blueprint={bp}")
-    a += bp["pk"] * best
-print("part a:", a)
-
-b = 1
-for bp in blueprints[:3]:
-    bfs = DFS(adj, max_depth=32)
-    bfs(Node())
-    best = max([s.n_geo for s, d in bfs.seen.items() if d == 32])
-    print(f"n_geodes={best} blueprint={bp}")
-    b *= best
-print("part b:", b)
+s0 = 0, 0, 0, 0, 0, 0, 0, 1
+print("part a:", sum(bp[0] * search([(2, *s0, 24), (3, *s0, 24)], bp) for bp in bps))
+print("part b:", math.prod(search([(2, *s0, 32), (3, *s0, 32)], bp) for bp in bps[:3]))
