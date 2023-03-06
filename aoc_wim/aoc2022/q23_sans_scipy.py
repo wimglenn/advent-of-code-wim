eb71058f804ba6ba235f320ad6ffbf8b1a5800b9
wim glenn <hey@wimglenn.com>
2023-03-06 01:34:16 -0600

    faster day 23 with scipy

---
+++ aoc_wim/aoc2022/q23_sans_scipy.py
@@ -0,0 +1,43 @@
+"""
+--- Day 23: Unstable Diffusion ---
+https://adventofcode.com/2022/day/23
+"""
+from aocd import data
+from aoc_wim.zgrid import ZGrid
+from collections import defaultdict
+from collections import deque
+from itertools import count
+
+
+grid = ZGrid(data, transform={".": None})
+edges = deque([
+    (-1j - 1, -1j, -1j + 1),
+    (+1j - 1, +1j, +1j + 1),
+    (-1j - 1,  -1, +1j - 1),
+    (-1j + 1,  +1, +1j + 1),
+])
+
+
+def movers():
+    d = defaultdict(list)  # dest: sources
+    for z0 in grid:
+        elves_nearby = {e[1]: sum(z0 + dz in grid for dz in e) for e in edges}
+        if sum(elves_nearby.values()):
+            for dz, n_nearby in elves_nearby.items():
+                if not n_nearby:
+                    d[z0 + dz].append(z0)
+                    break
+    return {z: zs[0] for z, zs in d.items() if len(zs) == 1}  # dest: source
+
+
+# grid.draw(title="== Initial State == ")
+for i in count(1):
+    z1_z0 = movers()
+    grid.d = dict.fromkeys(grid.keys() - z1_z0.values() | z1_z0.keys(), "#")
+    # grid.draw(title=f"== End of round {i} ==")
+    edges.rotate(-1)
+    if not z1_z0:  # nobody moved
+        print("part b:", i)
+        break
+    if i == 10:
+        print("part a:", grid.area - len(grid))
