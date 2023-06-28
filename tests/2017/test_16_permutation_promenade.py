228846bf704c58f03a2500dafcec419ff7c87792
wim glenn <hey@wimglenn.com>
2023-06-28 00:32:26 -0500

    2017/17 examples

--- tests/2017/test_16_permutation_promenade.py
+++
@@ -1,9 +0,0 @@
-from aoc_wim.aoc2017 import q16
-
-
-def test_example_a():
-    assert q16.dance(data="s1,x3/4,pe/b", d=list("abcde")) == "baedc"
-
-
-def test_example_b():
-    assert q16.dance(data="s1,x3/4,pe/b", d=list("abcde"), n=2) == "ceadb"
