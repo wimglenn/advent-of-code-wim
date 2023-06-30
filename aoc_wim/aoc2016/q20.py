de94ac72ee5641d13a0c1f30ffd9ad253301c9c3
wim glenn <hey@wimglenn.com>
2023-06-30 01:38:17 -0500

    2016 examples

--- aoc_wim/aoc2016/q20.py
+++ aoc_wim/aoc2016/q20.py
@@ -5,41 +5,29 @@ https://adventofcode.com/2016/day/20
 from aocd import data
 
 
-def cleanup_data(data):
-    intervals = []
-    for line in data.splitlines():
-        left, right = line.split("-")
-        interval = int(left), int(right)
-        intervals.append(interval)
-    intervals.sort()
+intervals = []
+for line in data.splitlines():
+    left, right = line.split("-")
+    interval = int(left), int(right)
+    intervals.append(interval)
+intervals.sort()
 
-    while True:
-        n = len(intervals)
-        for i in range(n - 1):
-            lo_p, hi_p = intervals[i]
-            lo, hi = intervals[i + 1]
-            if lo <= hi_p + 1:
-                del intervals[i + 1]
-                intervals[i] = lo_p, max(hi_p, hi)
-                break
-        else:
+while True:
+    n = len(intervals)
+    for i in range(n - 1):
+        lo_p, hi_p = intervals[i]
+        lo, hi = intervals[i + 1]
+        if lo <= hi_p + 1:
+            del intervals[i + 1]
+            intervals[i] = lo_p, max(hi_p, hi)
             break
+    else:
+        break
 
-    return intervals
+print("part a:", intervals[0][1] + 1)
 
+b = 4294967296 if len(intervals) > 3 else 10
+for lo, high in intervals:
+    b -= high - lo + 1
 
-def part_a(clean_data):
-    return clean_data[0][1] + 1
-
-
-def part_b(clean_data, n_min=0, n_max=4294967295):
-    n = n_max - n_min + 1
-    for lo, high in clean_data:
-        n -= high - lo + 1
-    return n
-
-
-if __name__ == "__main__":
-    clean_data = cleanup_data(data)
-    print(part_a(clean_data))
-    print(part_b(clean_data))
+print("part b:", b)
