836e19b282f0f27320a53265b99a049ad2740bc7
wim glenn <hey@wimglenn.com>
2023-07-01 13:45:46 -0500

    2016 examples

--- aoc_wim/aoc2016/q13.py
+++ aoc_wim/aoc2016/q13.py
@@ -4,30 +4,31 @@ https://adventofcode.com/2016/day/13
 """
 from aoc_wim.zgrid import ZGrid
 from aocd import data
+from functools import partial
 
 
-class WallMap:
-    def __init__(self, fav_number=None):
-        if fav_number is None:
-            fav_number = int(data)
-        self.fav_number = fav_number
+def wall(z, fav_number):
+    if z.real < 0 or z.imag < 0:
+        return "#"
+    x, y = z.real, z.imag
+    fz = x * x + 3 * x + 2 * x * y + y + y * y
+    popcount = bin(int(fz) + fav_number).count("1")
+    result = "#" if popcount % 2 else "."
+    return result
 
-    def __call__(self, z):
-        if z.real < 0 or z.imag < 0:
-            return "#"
-        x, y = z.real, z.imag
-        fz = x * x + 3 * x + 2 * x * y + y + y * y
-        popcount = bin(int(fz) + self.fav_number).count("1")
-        result = "#" if popcount % 2 else "."
-        return result
+
+def make_grid(data):
+    func = partial(wall, fav_number=int(data))
+    grid = ZGrid(func, on=".", off="#")
+    return grid
 
 
 z0 = 1 + 1j
-target = 31 + 39j
+target = 7 + 4j if data == "10" else 31 + 39j
 
 
 if __name__ == "__main__":
-    grid = ZGrid(WallMap(), on=".", off="#")
+    grid = make_grid(data)
     depths = grid.bfs(target=target, z0=z0)
     print("part a:", depths[target])
     depths = grid.bfs(z0=z0, max_depth=50)
--- tests/2016/test_13_a_maze_of_twisty_little_cubicles.py
+++ tests/2016/test_13_a_maze_of_twisty_little_cubicles.py
@@ -1,18 +1,11 @@
 from aoc_wim.aoc2016 import q13
 
 
-z0 = 1 + 1j
-target = 7 + 4j
+def test_render(capsys):
+    z0 = 1 + 1j
+    target = 7 + 4j
 
-
-def test_path_length():
-    grid = q13.ZGrid(q13.WallMap(fav_number=10), on=".", off="#")
-    path_length = grid.bfs(target, z0)[target]
-    assert path_length == 11
-
-
-def test_draw(capsys):
-    grid = q13.ZGrid(q13.WallMap(fav_number=10), on=".", off="#")
+    grid = q13.make_grid("10")
     grid.draw(window=9 + 6j, pretty=False)
     out, err = capsys.readouterr()
     expected1 = """
