bd1ab4b6e11651f5844c25f157b32909d0f70a7c
wim glenn <hey@wimglenn.com>
2023-12-05 03:06:27 -0600

    day 04

--- aoc_wim/aoc2023/q04.py
+++ aoc_wim/aoc2023/q04.py
@@ -5,14 +5,13 @@ https://adventofcode.com/2023/day/4
 from aocd import data
 
 lines = data.splitlines()
-n = len(lines)
-cards = [1] * n
+cards = [1] * len(lines)
 a = 0
 for i, line in enumerate(lines):
     L, R = line.split("|")
     w = {*L.split()} & {*R.split()}
     a += int(2 ** (len(w) - 1))
-    for j in range(min(len(w), n)):
+    for j in range(min(len(w), len(cards))):
         cards[i + 1 + j] += cards[i]
 
 print("answer_a:", a)
