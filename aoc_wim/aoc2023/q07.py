9d53b041e585aa4baff57aa36dbac833504fa1a7
wim glenn <hey@wimglenn.com>
2023-12-07 00:29:14 -0600

    day 07 (#88)
    * day 07
    
    * modest speedup by not replacing J

---
+++ aoc_wim/aoc2023/q07.py
@@ -0,0 +1,29 @@
+"""
+--- Day 7: Camel Cards ---
+https://adventofcode.com/2023/day/7
+"""
+from aocd import data
+from collections import Counter
+from itertools import combinations_with_replacement
+
+
+tr = str.maketrans("TQKA", "IKLM")
+
+
+def key_a(hand):
+    return sorted(Counter(hand).values(), reverse=True), hand.translate(tr)
+
+
+def key_b(hand):
+    hands = [hand]
+    for comb in combinations_with_replacement("AKQT98765432", hand.count("J")):
+        it = iter(comb)
+        hands.append("".join([next(it) if h == "J" else h for h in hand]))
+        assert next(it, None) is None
+    k0, _k1 = max(key_a(c) for c in hands)
+    return k0, hand.translate(tr).replace("J", "0")
+
+
+d = dict(h.split() for h in data.splitlines())
+print("answer_a", sum(i * int(d[k]) for i, k in enumerate(sorted(d, key=key_a), 1)))
+print("answer_b", sum(i * int(d[k]) for i, k in enumerate(sorted(d, key=key_b), 1)))
