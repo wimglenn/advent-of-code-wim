d04c2bac79412e77a7d87f44352fa06ef157b7bf
wim glenn <hey@wimglenn.com>
2023-12-06 22:59:44 -0600

    day 06

--- aoc_wim/aoc2023/q06.py
+++ aoc_wim/aoc2023/q06.py
@@ -2,17 +2,17 @@
 --- Day 6: Wait For It ---
 https://adventofcode.com/2023/day/6
 """
-from math import prod
+from math import prod, sqrt
 from aocd import data
 
 
-def solve(t, d):
-    return next(t + 1 - 2*v for v in range(t) if v*t - v*v > d)
+def quadratic(t, d):
+    return t - 1 - 2*int(t/2 - sqrt(t*t - 4*d)/2)
 
 
 ts, ds = data.splitlines()
 ts = ts.split()[1:]
 ds = ds.split()[1:]
 
-print("answer_a:", prod(solve(int(t), int(d)) for t, d in zip(ts, ds)))
-print("answer_b:", solve(int("".join(ts)), int("".join(ds))))
+print("answer_a:", prod(quadratic(int(t), int(d)) for t, d in zip(ts, ds)))
+print("answer_b:", quadratic(int("".join(ts)), int("".join(ds))))
