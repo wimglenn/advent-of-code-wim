d1dd0936e804493d60256ec22b0cb85767e7b3b5
wim glenn <hey@wimglenn.com>
2023-12-13 01:54:35 -0600

    day 13

---
+++ aoc_wim/aoc2023/q13_numpy.py
@@ -0,0 +1,22 @@
+"""
+--- Day 13: Point of Incidence ---
+https://adventofcode.com/2023/day/13
+"""
+from aocd import data
+import numpy as np
+
+a = b = 0
+for chunk in data.split("\n\n"):
+    h = chunk.count("\n") + 1
+    A = np.fromiter(chunk.replace("\n", ""), dtype="<U1").reshape(h, -1)
+    for A, f in zip([A, A.T], [100, 1]):
+        h = len(A)
+        for i in range(1, h):
+            A1 = A[max(2*i-h,0):i]
+            A2 = A[i:i+min(i,h-i)][::-1]
+            match (A1 != A2).sum():
+                case 0: a += f * i
+                case 1: b += f * i
+
+print("answer_a:", a)
+print("answer_b:", b)
