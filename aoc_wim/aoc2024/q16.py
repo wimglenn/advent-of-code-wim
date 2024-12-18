af350e295f4660adf16f20e64ed916c3a6d3c152
Wim Jeantine-Glenn <hey@wimglenn.com>
2024-12-18 02:32:34 -0600

    day 16

--- aoc_wim/aoc2024/q16.py
+++ aoc_wim/aoc2024/q16.py
@@ -8,46 +8,31 @@ from queue import PriorityQueue
 import itertools as it
 
 
-grid = ZGrid(data, on=".", off="#")
-grid.graph(extra="SE")
-z0 = grid.z("S")
-z1 = grid.z("E")
+grid = ZGrid(data)
 q = PriorityQueue()
 i = it.count()
-state = (0, next(i), z0, 1, [])
+S, E = grid.z("S"), grid.z("E")
+state = (0, next(i), S, 1, [S])
 q.put(state)
-while q:
-    score, _, z, dz, path = q.get()
+a = None
+score = {}
+paths = []
+while not q.empty():
+    s0, _, z0, dz0, path = q.get()
+    if s0 > score.get((z0, dz0), float("inf")):
+        continue
+    score[z0, dz0] = s0
+    if z0 == E:
+        if a is None:
+            a = s0
+        if a == s0:
+            paths.append(path)
+    for z in grid.near(z0):
+        if grid[z] == "#":
+            continue
+        dz = z - z0
+        s = s0 + [1001, 1][dz == dz0]
+        q.put((s, next(i), z, dz, path + [z]))
 
-
-# grid = ZGrid(data, on=".", off="#")
-# z0 = grid.z("S")
-# z1 = grid.z("E")
-# grid[z0] = "."
-# grid[z1] = "."
-# graph = grid.graph()
-#
-#
-# class Q16AStar(AStar):
-#     def cost(self, current_state, next_state):
-#         result = 1
-#         if current_state[1] != next_state[1]:
-#             result += 1000
-#         return result
-#
-#     def adjacent(self, state):
-#         z0, dz0 = state
-#         for z in grid.near(z0):
-#             if grid[z] == ".":
-#                 yield z, z - z0
-#
-#     def target_reached(self, current_state, target):
-#         return current_state[0] == target[0]
-#
-#
-# astar = Q16AStar((z0, 1), (z1, None))
-# paths = astar.run(first=0)
-# a = astar.gscore[paths[0][0]]
-# print("answer_a:", a)
-# b = len({z for path in paths for z, dz in path})
-# print("answer_b:", b)
+print("answer_a:", a)
+print("answer_b:", len(set().union(*paths)))
