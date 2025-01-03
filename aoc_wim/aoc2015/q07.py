7e008de027b3e387aff25ec68cc1071aef929044
Wim Jeantine-Glenn <hey@wimglenn.com>
2025-01-02 22:22:42 -0600

    day 7

--- aoc_wim/aoc2015/q07.py
+++ aoc_wim/aoc2015/q07.py
@@ -2,62 +2,39 @@
 --- Day 7: Some Assembly Required ---
 https://adventofcode.com/2015/day/7
 """
-import operator
 import re
+from collections import deque
 
 import numpy as np
 from aocd import data
 
 
-# TODO: change to use topological sort
-
-opmap = {
-    "AND": operator.and_,
-    "OR": operator.or_,
-    "LSHIFT": operator.lshift,
-    "RSHIFT": operator.rshift,
-}
-
-
 def compute(data):
-    result = {}
+    wire = {}
 
-    def getval(v):
-        return result[v] if v in result else np.uint16(v)
+    def get(v):
+        return wire[v] if v in wire else np.uint16(v)
 
-    lines = [line.split(" -> ") for line in data.splitlines()]
-
-    def process_line(line):
-        left, right = line
-        left = left.split()
-        len_left = len(left)
-        if len_left == 1:
-            # store
-            result[right] = getval(left[0])
-        elif len_left == 2:
-            # negation
-            op, val = left
-            if op != "NOT":
-                raise Exception
-            result[right] = ~getval(val)
-        elif len_left == 3:
-            a, op, b = left
-            op = opmap[op]
-            result[right] = op(getval(a), getval(b))
-
-    while lines:
-        line = lines.pop()
+    q = deque(data.splitlines())
+    while q:
+        line = q.pop()
         try:
-            process_line(line)
+            match line.split():
+                case n, "->", w:
+                    wire[w] = get(n)
+                case "NOT", n, "->", w:
+                    wire[w] = ~get(n)
+                case x, op, y, "->", w:
+                    f = getattr(np.uint16, f"__{op}__".lower())
+                    wire[w] = f(get(x), get(y))
         except (KeyError, ValueError):
-            lines = [line] + lines
+            q.appendleft(line)
 
-    return result
+    return wire["a"]
 
 
-result = compute(data)
-result_a = result["a"]
-print("answer_a:", result_a)
+a = compute(data)
+print("answer_a:", a)
 
-new_data = re.sub(r"\n([0-9]+) -> b\n", f"\n{result_a} -> b\n", data)
-print("answer_b:", compute(new_data)["a"])
+new_data = re.sub(r"\n([0-9]+) -> b\n", f"\n{a} -> b\n", data)
+print("answer_b:", compute(new_data))
