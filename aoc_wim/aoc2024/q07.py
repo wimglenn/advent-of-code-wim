a3535643452198e9d5d56fe3ce209f829faa294f
Wim Jeantine-Glenn <hey@wimglenn.com>
2025-01-05 15:18:46 -0600

    speedup 2024/07

--- aoc_wim/aoc2024/q07.py
+++ aoc_wim/aoc2024/q07.py
@@ -2,34 +2,34 @@
 --- Day 7: Bridge Repair ---
 https://adventofcode.com/2024/day/7
 """
-import itertools as it
+from math import log10
 
 from aocd import data
 
 
-def concat(n1, n2):
-    return int(str(n1) + str(n2))
+def possible(target, ns, part="a"):
+    *ns, n = ns
+    if not ns:
+        return n == target
+    if part == "b":
+        q, r = divmod(target, 10 ** (1 + int(log10(n))))
+        if r == n and possible(q, ns, part):
+            return True
+    q, r = divmod(target, n)
+    if r == 0 and possible(q, ns, part):
+        return True
+    return possible(target - n, ns, part)
 
 
 a = b = 0
 for line in data.replace(":", "").splitlines():
-    result, *ns = map(int, line.split())
-    for ops in it.product((int.__add__, int.__mul__), repeat=len(ns)-1):
-        val = ns[0]
-        for i, f in enumerate(ops, 1):
-            val = f(val, ns[i])
-        if val == result:
-            a += result
-            b += result
-            break
-    else:
-        for ops in it.product((int.__add__, int.__mul__, concat), repeat=len(ns) - 1):
-            val = ns[0]
-            for i, f in enumerate(ops, 1):
-                val = f(val, ns[i])
-            if val == result:
-                b += result
-                break
+    target, *ns = map(int, line.split())
+    if possible(target, ns):
+        a += target
+        b += target
+    elif possible(target, ns, part="b"):
+        b += target
+
 
 print("answer_a:", a)
 print("answer_b:", b)
