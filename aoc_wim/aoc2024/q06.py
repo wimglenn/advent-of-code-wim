0e94181541d52e36edc8ff72ae6cef795e9ea8f2
Wim Jeantine-Glenn <hey@wimglenn.com>
2025-01-06 22:44:54 -0600

    speedup 2024/06

--- aoc_wim/aoc2024/q06.py
+++ aoc_wim/aoc2024/q06.py
@@ -7,29 +7,33 @@ from aocd import data
 from aoc_wim.zgrid import ZGrid
 
 
-def walk():
-    z, dz = z0, dz0
-    seen = {(z, dz)}
+def walk(z, dz):
+    n = 0
+    path = {(z, dz): n}
+    cyclic = False
     while z + dz in grid:
         while grid[z + dz] == "#":
             dz *= 1j
         z += dz
-        if (z, dz) in seen:
-            return seen, True
-        seen.add((z, dz))
-    return seen, False
+        n += 1
+        if (z, dz) in path:
+            cyclic = True
+            break
+        path[z, dz] = n
+    return path, cyclic
 
 
 grid = ZGrid(data)
-z0, dz0 = grid.z("^"), -1j
-seen, _ = walk()
-path = dict(seen)
-print("answer_a:", len(path))
+path, _cyclic = walk(grid.z("^"), -1j)
+path = list(path)
+print("answer_a:", len(dict(path)))
 
 b = 0
-path.pop(z0)
-for z in path:
-    grid[z] = "#"
-    b += walk()[1]
-    grid[z] = "."
+seen = {path[0][0]}
+for (z0, dz0), (z1, _) in zip(path, path[1:]):
+    if z1 not in seen:
+        grid[z1] = "#"
+        b += walk(z0, dz0)[1]
+        grid[z1] = "."
+        seen.add(z1)
 print("answer_b:", b)
