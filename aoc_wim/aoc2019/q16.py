4e091cdb0b4ce4ac83627fedd96ef48d294c8686
Wim Jeantine-Glenn <hey@wimglenn.com>
2025-01-08 22:12:42 -0600

    2019/16 better examples

--- aoc_wim/aoc2019/q16.py
+++ aoc_wim/aoc2019/q16.py
@@ -6,6 +6,7 @@ from itertools import cycle
 
 import numpy as np
 from aocd import data
+from aocd import extra
 
 
 def square_wave(w=1):
@@ -15,18 +16,18 @@ def square_wave(w=1):
     return gen
 
 
-def part_a(data, n=100):
+def part_a(data):
     x = np.fromiter(data, int)
     [w] = x.shape
     waves = [square_wave(i + 1) for i in range(w)]
     A = np.array([[next(g) for _ in range(w)] for g in waves], dtype=int)
-    for _ in range(n):
+    for _ in range(extra.get("iterations", 100)):
         x = abs((A * x).sum(axis=1)) % 10
     result = ''.join(str(n) for n in x[:8])
     return result
 
 
-def part_b(data, n=100):
+def part_b(data):
     x = np.fromiter(data, int)
     [w] = x.shape
     pos = int(data[:7])
@@ -36,7 +37,7 @@ def part_b(data, n=100):
         return
     X = np.hstack([x] * stack)
     X = X[pos_r:]
-    for _ in range(n):
+    for _ in range(extra.get("iterations", 100)):
         X = X[::-1].cumsum()[::-1] % 10
     return ''.join(str(n) for n in list(X)[pos_r:pos_r+8])
 
--- tests/2019/test_16_flawed_frequency_transmission.py
+++
@@ -1,8 +0,0 @@
-from aoc_wim.aoc2019 import q16
-
-
-def test_small_example():
-    assert q16.part_a("12345678", 1) == "48226158"
-    assert q16.part_a("12345678", 2) == "34040438"
-    assert q16.part_a("12345678", 3) == "03415518"
-    assert q16.part_a("12345678", 4) == "01029498"
