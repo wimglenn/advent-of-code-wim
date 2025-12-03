f7b2d244042797538fc519049960cecfc1a2fd8d
Wim Jeantine-Glenn <hey@wimglenn.com>
2025-12-02 23:31:05 -0600

    day 3

---
+++ aoc_wim/aoc2025/q03.py
@@ -0,0 +1,22 @@
+"""
+--- Day 3: Lobby ---
+https://adventofcode.com/2025/day/3
+"""
+
+from aocd import data
+
+
+def max_jolts(line, d=2):
+    s = []
+    i = 0
+    while d:
+        m = max(line[i : len(line) - d + 1])
+        s.append(m)
+        i = line.index(m, i) + 1
+        d -= 1
+    return int("".join(s))
+
+
+lines = data.split()
+print("answer_a:", sum(max_jolts(x, d=2) for x in lines))
+print("answer_b:", sum(max_jolts(x, d=12) for x in lines))
