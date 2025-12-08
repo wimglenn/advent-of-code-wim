a5dd9fd2517f82e071fab88fe9680c61207826e0
Wim Jeantine-Glenn <hey@wimglenn.com>
2025-12-08 00:32:34 -0600

    refuktor

--- aoc_wim/aoc2025/q08.py
+++ aoc_wim/aoc2025/q08.py
@@ -20,24 +20,18 @@ for i in range(n):
         d2[frozenset([i, j])] = dx**2 + dy**2 + dz**2
 d2 = {k: d2[k] for k in sorted(d2, key=d2.get, reverse=True)}
 
-
-def merge(d2, components):
+components = {i: {i} for i in range(n)}
+pair = 0
+while True:
     (i, j), _d = d2.popitem()
     component = components[i] | components[j]
     for x in component:
         components[x] = component
-    return i, j
-
-
-n_pairs = extra.get("n_pairs", 1000)
-components = {i: {i} for i in range(n)}
-for _ in range(n_pairs):
-    merge(d2, components)
-
-unique_components = {frozenset(x) for x in components.values()}
-sizes = sorted([len(x) for x in unique_components], reverse=True)
-print("answer_a:", sizes[0] * sizes[1] * sizes[2])
-
-while len({id(x) for x in components.values()}) > 1:
-    i, j = merge(d2, components)
-print("answer_b:", xs[i] * xs[j])
+    pair += 1
+    if pair == extra.get("n_pairs", 1000):
+        unique_components = {frozenset(x) for x in components.values()}
+        sizes = sorted([len(x) for x in unique_components], reverse=True)
+        print("answer_a:", sizes[0] * sizes[1] * sizes[2])
+    if len(component) == n:
+        print("answer_b:", xs[i] * xs[j])
+        break
